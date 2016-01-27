266051882bf208d5e809512d82a6357634986639
Antonio Espinosa <antespi@gmail.com>
2016-01-27 13:54:21 +0100

    [IMP] Copy partner payment mode to contracts when installing

--- contract_payment_mode/__init__.py
+++ contract_payment_mode/__init__.py
@@ -1,2 +1,6 @@
 # -*- coding: utf-8 -*-
+# © 2016 Antiun Ingenieria S.L. - Antonio Espinosa
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from . import models
+from .hooks import post_init_hook
