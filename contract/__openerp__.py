6a5446bfabc8249899d184f752b4e9dd33cc2e5b
Carlos Incaser <carlos@incaser.es>
2016-03-22 20:34:18 +0100

    [MIG] contract: from v7 to v9 improving features
    * Tests added
    * Filters added.
    * Add _id/_ids in old property fields
    * Translations

--- contract/__openerp__.py
+++ contract/__openerp__.py
@@ -1,51 +1,24 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
+# © 2004-2010 OpenERP SA
+# © 2016 Carlos Dauden <carlos.dauden@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Contracts Management recurring',
-    'version': '0.1',
+    'version': '9.0.1.0.0',
     'category': 'Other',
-    'description': """
-This module adds a new feature in contracts to manage recurring invoicing
-=========================================================================
-
-This is a backport of the new V8 feature available in trunk and saas. With
-the V8 release this module will be deprecated.
-
-It also adds a little feature, you can use #START# and #END# in the contract
-line description to automatically insert the dates of the invoiced period.
-
-Backport done By Yannick Buron.
-""",
-    'author': "OpenERP SA,Odoo Community Association (OCA)",
+    'license': 'AGPL-3',
+    'author': "OpenERP SA,"
+              "Tecnativa,"
+              "Odoo Community Association (OCA)",
     'website': 'http://openerp.com',
-    'depends': ['base', 'account_analytic_analysis'],
+    'depends': ['base', 'account', 'analytic'],
     'data': [
         'security/ir.model.access.csv',
-        'account_analytic_analysis_recurring_cron.xml',
-        'account_analytic_analysis_recurring_view.xml',
+        'data/contract_cron.xml',
+        'views/contract.xml',
+        'views/account_invoice_view.xml',
     ],
-    'demo': [],
-    'test': [],
     'installable': True,
     'images': [],
 }
