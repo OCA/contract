6001ea9eb9cc16fea57a59f41eb4c668b348b6d4
Carlos Incaser <carlos@incaser.es>
2016-04-01 11:45:34 +0200

    [FIX] contract: Update author info.

--- contract/tests/__init__.py
+++ contract/tests/__init__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-# (c) 2015 Antiun Ingeniería S.L. - Sergio Teruel
-# (c) 2015 Antiun Ingeniería S.L. - Carlos Dauden
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+# © 2016 Carlos Dauden <carlos.dauden@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import test_contract
