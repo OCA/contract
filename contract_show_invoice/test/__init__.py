99fd1b7be4235882c2e9db34d94d62ac603ccc0d
carlosdauden <carlos@incaser.es>
2016-04-02 19:02:27 +0200

    [MIG] contract_show_invoice: Migrate to v9.0
    * [MIG] contract_show_invoice: Migrate to v9.0

---
+++ contract_show_invoice/test/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# © 2016 Carlos Dauden <carlos.dauden@tecnativa.com>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from . import test_contract_show_invoice
