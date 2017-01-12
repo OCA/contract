d3addda52b50e2dbd0a8e77bfe5b91adefeaf6f2
Ted Salmon <tsalmon@laslabs.com>
2017-01-12 13:16:25 -0800

    [ADD] product_contract: Create module * Add contract functionality to `product.templates` * Add logic to create contracts from `sale.order` that contains contract products.

---
+++ product_contract/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 LasLabs Inc.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import product_template
+from . import sale_order
+from . import sale_order_line
