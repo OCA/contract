d135cc2aa9e6c3b5893aa18b5ae261bc77e69ab5
cubells <vicent@vcubells.net>
2017-03-28 14:52:12 +0200

    contract_payment_mode: Migration to 9.0

--- contract_payment_mode/models/contract.py
+++ contract_payment_mode/models/contract.py
@@ -1,33 +1,27 @@
 # -*- coding: utf-8 -*-
-from openerp import models, fields, api
+from openerp import api, fields, models
 
 
 class AccountAnalyticAccount(models.Model):
     _inherit = 'account.analytic.account'
 
     payment_mode_id = fields.Many2one(
-        'payment.mode',
+        comodel_name='account.payment.mode',
         string='Payment Mode',
-        domain="[('sale_ok', '=', True)]")
+        domain=[('payment_type', '=', 'inbound')],
+    )
 
-    @api.multi
-    def on_change_partner_id(self, partner_id, name):
-        res = super(AccountAnalyticAccount, self).on_change_partner_id(
-            partner_id,
-            name)
-        partner = self.env['res.partner'].browse(partner_id)
-        if partner and partner.customer_payment_mode:
-            res['value']['payment_mode_id'] = partner.customer_payment_mode.id
-        return res
+    @api.onchange('partner_id')
+    def on_change_partner_id(self):
+        if self.partner_id.customer_payment_mode_id:
+            self.payment_mode_id = self.partner_id.customer_payment_mode_id.id
 
     @api.model
     def _prepare_invoice_data(self, contract):
-        invoice_vals = super(AccountAnalyticAccount, self).\
-            _prepare_invoice_data(contract)
+        invoice_vals = super(AccountAnalyticAccount, self)._prepare_invoice()
         if contract.payment_mode_id:
             invoice_vals['payment_mode_id'] = contract.payment_mode_id.id
             invoice_vals['partner_bank_id'] = (
-                contract.partner_id.bank_ids[:1].id or
-                contract.payment_mode_id.bank_id.id)
-
+                contract.partner_id.bank_ids[:1].id
+            )
         return invoice_vals
