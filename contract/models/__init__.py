c05caf88d7f8e11dbafa7425a48a3b0d4df97cfe
Dave Lasley <dave@laslabs.com>
2017-04-01 08:14:55 -0700

    [IMP][10.0] contract: Add templating (#42)
    Add template functionality for contracts

--- contract/models/__init__.py
+++ contract/models/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-# © 2016 Carlos Dauden <carlos.dauden@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import contract
-from . import invoice
+from . import account_analytic_contract
+from . import account_analytic_account
+from . import account_analytic_invoice_line
+from . import account_invoice
