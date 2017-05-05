c197faf44637ac2cb8005f297b34585d88ce0e15
Dave Lasley <dave@laslabs.com>
2017-05-05 13:43:25 -0700

    [ADD] contract_auto: Automatic Payment For Contracts

---
+++ contract_payment_auto/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 LasLabs Inc.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Contract - Auto Payment",
+    "summary": "Adds automatic payments to contracts.",
+    "version": "10.0.1.0.0",
+    "category": "Contract Management",
+    "license": "AGPL-3",
+    "author": "LasLabs, "
+              "Odoo Community Association (OCA)",
+    "website": "https://laslabs.com",
+    "depends": [
+        "contract",
+        "payment",
+    ],
+    "data": [
+        "data/mail_template_data.xml",
+        "data/ir_cron_data.xml",
+        "views/account_analytic_account_view.xml",
+        "views/account_analytic_contract_view.xml",
+        "views/res_partner_view.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
---
+++ contract_payment_auto/models/account_invoice.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 LasLabs Inc.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    auto_pay_attempts = fields.Integer()
+    auto_pay_failed = fields.Datetime()
