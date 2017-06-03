1177dedf375c30abb37f3353f0d4265ba494029b
Carlos Dauden <carlos.dauden@tecnativa.com>
2017-06-03 15:02:07 +0200

    [9.0][ADD] contract_mandate: New module

---
+++ contract_mandate/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Carlos Dauden - Tecnativa <carlos.dauden@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Contract Mandate',
+    'summary': 'Mandate in contracts and their invoices',
+    'version': '9.0.1.0.0',
+    'author': 'Tecnativa, '
+              'Odoo Community Association (OCA)',
+    'website': 'http://www.tecnativa.com',
+    'depends': [
+        'contract_payment_mode',
+        'account_banking_mandate',
+    ],
+    'category': 'Sales Management',
+    'license': 'AGPL-3',
+    'data': [
+        'views/contract_view.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+}
