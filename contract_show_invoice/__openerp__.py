7af3b7614842a114163a7f014bad89f07b1ca992
Carlos Dauden <carlos.dauden@tecnativa.com>
2017-08-03 18:03:49 +0200

    [9.0][FIX] contract_show_invoice: Error if create invoice with viex context

--- contract_show_invoice/__openerp__.py
+++ contract_show_invoice/__openerp__.py
@@ -14,6 +14,7 @@
     'depends': ['account', 'analytic'],
     'category': 'Sales Management',
     'data': [
+        'views/account_invoice_view.xml',
         'views/contract_view.xml',
     ],
     'installable': True,
