457db55a3b78f23d6cc0d75ec21bbf375b76590b
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-08-22 11:54:13 +0200

    [FIX] contract: fix views assignment + improvement on tests + copyright in headers
    **Be totally deterministic about which views to use**
    
    Having a primary view that is not explicitly declared to be uses and w/o priority
    makes Odoo to choose between one of them randomly (well, not exactly, but kind of),
    so we put here which views to use.
    
    I have also put tree view as primary and put a large priority for not being
    selected on other actions that don't have this explicit views.
    
    A friendly name in views is also assigned.
    
    **Improvements in tests**
    
    * Use SavepointCase for making the setup only once for all tests
    * Make them inheritable, creating a base class with only the setup,
      so that it can be inherited without the need of executing all tests
      contained here each time you inherit it, and adding other class
      in the same module that inherits from the base class that actually
      performs the tests.
    * Removed duplicated test method

--- contract/__manifest__.py
+++ contract/__manifest__.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
-# © 2004-2010 OpenERP SA
-# © 2016 Carlos Dauden <carlos.dauden@tecnativa.com>
+# Copyright 2004-2010 OpenERP SA
+# Copyright 2014-2017 Tecnativa - Pedro M. Baeza
+# Copyright 2015 Domatix
+# Copyright 2016 Tecnativa - Carlos Dauden
+# Copyright 2017 Tecnativa - Vicent Cubells
+# Copyright 2016-2017 LasLabs Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Contracts Management - Recurring',
-    'version': '10.0.2.0.0',
+    'version': '10.0.2.1.0',
     'category': 'Contract Management',
     'license': 'AGPL-3',
     'author': "OpenERP SA, "
