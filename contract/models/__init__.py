6cd28264b47504a8311792add9966c5c8461dcf1
Dave Lasley <dave@laslabs.com>
2017-09-25 05:14:21 -0700

    [FIX] contract: Template lines handling (#92)
    Update contract template lines handling to fix #80, and fix #59 #100

--- contract/models/__init__.py
+++ contract/models/__init__.py
@@ -4,4 +4,5 @@
 from . import account_analytic_contract
 from . import account_analytic_account
 from . import account_analytic_invoice_line
+from . import account_analytic_contract_line
 from . import account_invoice
