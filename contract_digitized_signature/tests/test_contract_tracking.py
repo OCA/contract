223d859c1c1c7aa425712b4f1d0e68624fd20221
Brett Wood <bwood@laslabs.com>
2017-10-24 21:07:06 -0700

    [MIG] contract_digitized_signature: Mig to 10.0.

--- contract_digitized_signature/tests/test_contract_tracking.py
+++ contract_digitized_signature/tests/test_contract_tracking.py
@@ -2,7 +2,7 @@
 # Copyright 2017 Tecnativa - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp.tests import common
+from odoo.tests import common
 
 
 class TestContractSignatureTracking(common.SavepointCase):
