e12d950320ff74d781aff14f01b533486877dc5c
Carlos Dauden <carlos.dauden@tecnativa.com>
2017-11-03 17:59:16 +0100

    [MIG] contract_mandate: Migration to 10.0

--- contract_mandate/__manifest__.py
+++ contract_mandate/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': 'Contract Mandate',
     'summary': 'Mandate in contracts and their invoices',
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'author': 'Tecnativa, '
               'Odoo Community Association (OCA)',
     'website': 'http://www.tecnativa.com',
