4237f3800351d2c0deca85d9874dd22a319fd405
Carlos Dauden <carlos.dauden@tecnativa.com>
2017-11-08 15:51:32 +0100

    [IMP] contract: Module contract improvements (#107)

--- contract/models/__init__.py
+++ contract/models/__init__.py
@@ -6,3 +6,4 @@ from . import account_analytic_account
 from . import account_analytic_invoice_line
 from . import account_analytic_contract_line
 from . import account_invoice
+from . import res_partner
