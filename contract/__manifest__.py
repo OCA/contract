01d3e99bc1d21a5def78dc9c03f2cea0d12ffb14
Pedro Castro Silva <pedrocasi@users.noreply.github.com>
2017-11-18 18:25:25 +0000

    [IMP] contract: Add display sequence on contract lines (#119)

--- contract/__manifest__.py
+++ contract/__manifest__.py
@@ -9,7 +9,7 @@
 
 {
     'name': 'Contracts Management - Recurring',
-    'version': '10.0.3.0.0',
+    'version': '10.0.3.1.0',
     'category': 'Contract Management',
     'license': 'AGPL-3',
     'author': "OpenERP SA, "
