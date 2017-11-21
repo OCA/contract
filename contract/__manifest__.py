48a1dd7d405f7f8da5be665032d3a638ca4cab06
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-11-21 19:16:06 +0100

    [IMP] contract: Menu position

--- contract/__manifest__.py
+++ contract/__manifest__.py
@@ -8,7 +8,7 @@
 
 {
     'name': 'Contracts Management - Recurring',
-    'version': '11.0.1.0.0',
+    'version': '11.0.1.1.0',
     'category': 'Contract Management',
     'license': 'AGPL-3',
     'author': "OpenERP SA, "
