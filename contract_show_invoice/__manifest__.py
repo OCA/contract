b78ee0ea22111ed71cec8c883a5d8e95c05b265b
Sergio Zanchetta <primes2h@users.noreply.github.com>
2017-12-02 16:47:09 +0100

    [FIX] contract_show_invoice: wrong value for account (#99)

--- contract_show_invoice/__manifest__.py
+++ contract_show_invoice/__manifest__.py
@@ -6,7 +6,8 @@
 {
     'name': 'Contract Show Invoice',
     'summary': 'Button in contracts to show their invoices',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.0.1',
+    'license': 'AGPL-3',
     'author': 'Domatix,'
               'Tecnativa,'
               'Odoo Community Association (OCA)',
