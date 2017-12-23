5acff0efbee55d6bcc4a083738ecc13fdee4834d
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-12-23 02:04:26 +0100

    [IMP] contract: Automatic prices for lines

--- contract/__manifest__.py
+++ contract/__manifest__.py
@@ -9,7 +9,7 @@
 
 {
     'name': 'Contracts Management - Recurring',
-    'version': '10.0.3.3.0',
+    'version': '10.0.4.0.0',
     'category': 'Contract Management',
     'license': 'AGPL-3',
     'author': "OpenERP SA, "
