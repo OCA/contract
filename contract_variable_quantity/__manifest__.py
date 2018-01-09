e4e2b9f82aef43d8e050a61a5dc0bb35b00f1909
Carlos Dauden <carlos.dauden@tecnativa.com>
2018-01-09 18:07:13 +0100

    [MIG] contract_variable_quantity: Migration to 11.0

--- contract_variable_quantity/__manifest__.py
+++ contract_variable_quantity/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # © 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Variable quantity in contract recurrent invoicing',
-    'version': '10.0.1.1.0',
+    'version': '11.0.1.0.0',
     'category': 'Contract Management',
     'license': 'AGPL-3',
     'author': "Tecnativa,"
