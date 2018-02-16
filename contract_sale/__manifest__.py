b22935bcf4e76bcbbb66be52c7f112c7e890c6a4
Levent Karakaş <leventk@eskayazilim.com.tr>
2018-02-16 13:57:43 +0300

    [MIG] contract_sale: Migration to 11.0

--- contract_sale/__manifest__.py
+++ contract_sale/__manifest__.py
@@ -1,16 +1,15 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Tecnativa - Luis M. Ontalba
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
     'name': 'Contract from Sale',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Sales',
     'author': 'Tecnativa, '
               'Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/contract',
     'depends': [
-        'sales_team',
+        'sale_management',
         'contract',
     ],
     'data': [
