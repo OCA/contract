386699c3a37b0d2a9f8c83747013850923cf0edc
Levent Karakaş <leventk@eskayazilim.com.tr>
2018-02-17 17:40:35 +0300

    migration to 11.0

--- contract_show_invoice/__manifest__.py
+++ contract_show_invoice/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # © 2015 Angel Moya <angel.moya@domatix.com>
 # © 2016 Carlos Dauden <carlos.dauden@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
@@ -6,7 +5,7 @@
 {
     'name': 'Contract Show Invoice',
     'summary': 'Button in contracts to show their invoices',
-    'version': '10.0.1.0.1',
+    'version': '11.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'Domatix,'
               'Tecnativa,'
