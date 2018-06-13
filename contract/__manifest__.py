0511acfb68a687343185917d66c12066c242f0a3
Carlos Dauden <carlos.dauden@tecnativa.com>
2018-06-13 14:37:23 +0200

    [FIX] contract: Custom values overwrited by xml, missed noupdate

--- contract/__manifest__.py
+++ contract/__manifest__.py
@@ -8,7 +8,7 @@
 
 {
     'name': 'Contracts Management - Recurring',
-    'version': '11.0.2.0.2',
+    'version': '11.0.2.1.0',
     'category': 'Contract Management',
     'license': 'AGPL-3',
     'author': "OpenERP SA, "
