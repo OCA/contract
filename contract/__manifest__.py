f85a2a0f341bae482e15d7d614876ddd54b06ed0
Dmytro Katyukha <firemage.dima@gmail.com>
2018-06-27 00:40:12 +0300

    [FIX] analytic invoice/contract lines inheritance
    Bug description
    ---------------
    
    `account.analytic.contract.line` inherits
    `account.analytic.invoice.line`
    
    `account.analytic.invoice.line` defines field `analytic_account_id`:
       - comodel='account.analytic.account'
    
    `account.analytic.contract.line` redefines field `analytic_account_id`:
       - comodel='account.analytic.contract'
    
    On attempt to extend `account.analytic.invoice.line` model adding
    field that depends on `analytic_account_id.date_start`
    Odoo fails to update, because it adds this field to
    `account.analytic.contract.line` through inheritance,
    and `account.analytic.contract` model have no this field.
    
    What is done
    ------------
    
    Change inheritance order:
    - `account.analytic.invoice.line` inherits
    `account.analytic.contract.line`
    - no file renames at this stage (this wil be done in next commit)

--- contract/__manifest__.py
+++ contract/__manifest__.py
@@ -8,7 +8,7 @@
 
 {
     'name': 'Contracts Management - Recurring',
-    'version': '11.0.2.1.0',
+    'version': '11.0.3.0.0',
     'category': 'Contract Management',
     'license': 'AGPL-3',
     'author': "OpenERP SA, "
