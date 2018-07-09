3550a8add0f61efe14660fd5b207cbeb38f88212
Jon Erik Ceberio <jonerikceberio@digital5.es>
2018-07-09 12:05:22 +0200

    [IMP] contract: possible error in migration if the column does not exist

--- contract/migrations/11.0.2.0.0/pre-migration.py
+++ contract/migrations/11.0.2.0.0/pre-migration.py
@@ -7,7 +7,12 @@ def migrate(cr, version):
     """Rename column for specific price for keeping backwards compatibility."""
     if not version:
         return
-    cr.execute(
-        "ALTER TABLE account_analytic_invoice_line "
-        "RENAME price_unit TO specific_price"
-    )
+    cr.execute("""SELECT column_name
+        FROM information_schema.columns
+        WHERE table_name='account_analytic_invoice_line' AND
+        column_name='price_unit'""")
+    if cr.fetchone():
+        cr.execute(
+            "ALTER TABLE account_analytic_invoice_line "
+            "RENAME price_unit TO specific_price"
+        )
