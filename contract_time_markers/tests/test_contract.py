bf04f4299a4ba6b6d9be692716405eb2dc328829
Roel Adriaans <roel@road-support.nl>
2018-07-21 21:50:02 +0200

    [ADD] Add copyright to tests

--- contract_time_markers/tests/test_contract.py
+++ contract_time_markers/tests/test_contract.py
@@ -1,5 +1,6 @@
 # Copyright 2016 Tecnativa - Carlos Dauden
 # Copyright 2017 Tecnativa - Pedro M. Baeza
+# Copyright 2018 Road-Support - Roel Adriaans
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import fields
