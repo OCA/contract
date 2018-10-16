9f5178e9eb1e61753b1a82962b2e23bbd9dd7f08
Bhavesh Odedra <bodedra@ursainfosystems.com>
2018-10-16 17:20:33 +0530

    [ENH] OCA Standards

--- agreement/models/res_partner.py
+++ agreement/models/res_partner.py
@@ -1,7 +1,14 @@
-from odoo import models, fields, api
+# Copyright (C) 2018 - TODAY, Pavlov Media
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields
+
 
 class Partner(models.Model):
-
     _inherit = 'res.partner'
 
-    agreements = fields.One2many('partner_agreement.agreement', 'name', string="Agreements")
+    agreements_ids = fields.One2many(
+        'agreement',
+        'name',
+        string="Agreements"
+    )
