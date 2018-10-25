96c34c1fee63c5207b5c07b6670c47694e65e4ce
sbejaoui <souheil.bejaoui@acsone.eu>
2018-10-25 17:49:32 +0200

    [MIG] contract_sale_invoicing: Migration to 12.0

---
+++ contract_sale_invoicing/readme/USAGE.rst
@@ -0,0 +1,8 @@
+To use this module, you need to:
+
+#. Go to Invoicing > Sales > Contracts and select or create a new contract.
+#. Check *Generate recurring invoices automatically*.
+#. Mark the check "Invoice Pending Sales Orders".
+#. On each invoicing, system will check if there's any pending sales orders
+   with same analyitic account and will append the lines to the invoice being
+   generated.
--- contract_sale_invoicing/tests/test_contract_sale_invoicing.py
+++ contract_sale_invoicing/tests/test_contract_sale_invoicing.py
@@ -9,7 +9,8 @@ class TestContractSaleInvoicing(TestContractBase):
     def setUpClass(cls):
         super(TestContractSaleInvoicing, cls).setUpClass()
         cls.product_so = cls.env.ref(
-            'product.service_order_01_product_template')
+            'product.product_product_1')
+        cls.product_so.invoice_policy = 'order'
         cls.sale_order = cls.env['sale.order'].create({
             'partner_id': cls.partner.id,
             'partner_invoice_id': cls.partner.id,
