6cd5abf87613bb571f7635d0700ac10e55722692
sbejaoui <souheil.bejaoui@acsone.eu>
2018-10-25 17:49:32 +0200

    [MIG] contract_sale_invoicing: Migration to 12.0

--- contract_sale_invoicing/__manifest__.py
+++ contract_sale_invoicing/__manifest__.py
@@ -3,7 +3,7 @@
 {
     'name': 'Contract Invoicing of Pending Sales Orders',
     'summary': 'Include sales to invoice in contract invoice creation',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Contract Management',
     'website': 'https://github.com/oca/contract',
     'author': 'Tecnativa, '
---
+++ contract_sale_invoicing/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This modules allows to include on the same invoice than the recurring invoice
+contract, all the pending to invoice sales orders that you have with the same
+analytic account.
