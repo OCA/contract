23d42d6974b258a229452400a720d3f215da7cd2
sbejaoui <souheil.bejaoui@acsone.eu>
2018-10-30 18:50:41 +0100

    [IMP] - Make recurrence mechanism on contract line
    Make recurrence mechanism on contract line and some other refactoring
    
    [FIX] - Keep contract_cron on account_analytic_account model
    
    contract_cron defined with no_update option.
    Changing it, will cause issue to past version installation.
    
    [IMP] - Fix recurring_next_date default value
    
    recurring_next_date should have start_date as default value in prepaid policy
    and start_date + invoicing_interval if postpaid
    
    [FIX] - Fix test check no journal
    
    [IMP] - Return created invoices on recurring_create_invoice
    
    [IMP] - Specific process to compute recurring_next_date for  monthly-last-day
    
    fixes: #198
    
    [ADD] - Add Post-migration script to bring recurrence info from contract to contract lines
    
    [ADD] - Add search filter based on date_end and recurring_next_date
    
     - not_finished filter in contract search view
     - finished filter in contract search view
     - Next Invoice group by in contract search view
    
    [ADD] - Add unit tests
    
    - cases to compute first recurring next date
    - contract recurring_next_date
    - contract date_end
    
    [IMP] - Improve Unit tests

---
+++ contract/models/contract_template_line.py
@@ -0,0 +1,38 @@
+# Copyright 2004-2010 OpenERP SA
+# Copyright 2014 Angel Moya <angel.moya@domatix.com>
+# Copyright 2015 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2016-2018 Carlos Dauden <carlos.dauden@tecnativa.com>
+# Copyright 2016-2017 LasLabs Inc.
+# Copyright 2018 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class AccountAnalyticContractLine(models.Model):
+    _name = 'account.analytic.contract.line'
+    _inherit = 'account.abstract.analytic.contract.line'
+    _description = 'Contract Lines'
+    _order = "sequence,id"
+
+    contract_template_id = fields.Many2one(
+        string='Contract',
+        comodel_name='account.analytic.contract',
+        required=True,
+        ondelete='cascade',
+        oldname='analytic_account_id',
+    )
+    partner_id = fields.Many2one(
+        comodel_name="res.partner",
+        string="Partner (always False)",
+        related='contract_template_id.partner_id',
+        store=True,
+        readonly=True,
+    )
+    pricelist_id = fields.Many2one(
+        comodel_name='product.pricelist',
+        string='Pricelist',
+        related='contract_template_id.pricelist_id',
+        store=True,
+        readonly=True,
+    )
