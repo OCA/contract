214c4de11c04e486cd0a1b96b4e7e6888f8c5616
sbejaoui <souheil.bejaoui@acsone.eu>
2018-10-31 16:21:51 +0100

    [MIG] - Product Contract Migration to 12.0

--- product_contract/__manifest__.py
+++ product_contract/__manifest__.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 LasLabs Inc.
+# Copyright 2018 ACSONE SA/NV.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Product Contract',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Contract Management',
     'license': 'AGPL-3',
     'author': "LasLabs, "
+              "ACSONE SA/NV, "
               "Odoo Community Association (OCA)",
     'website': 'https://laslabs.com',
     'depends': [
