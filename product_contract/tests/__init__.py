4b260d57bba991aa5d332033b8f178ad228c24ce
sbejaoui <souheil.bejaoui@acsone.eu>
2018-10-31 16:23:34 +0100

    [IMP] - Product with is_contract can be only of type service

--- product_contract/tests/__init__.py
+++ product_contract/tests/__init__.py
@@ -2,5 +2,5 @@
 # Copyright 2017 LasLabs Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import test_product_template
+from . import test_product
 from . import test_sale_order
