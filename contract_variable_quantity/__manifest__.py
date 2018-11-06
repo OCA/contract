ff926e1f808f0bbee729caba971f2c864335e876
sbejaoui <souheil.bejaoui@acsone.eu>
2018-11-06 12:22:08 +0100

    [MIG] contract_variable_quantity: Migration to 12.0
    [IMP] Adapt unit test
    
    [ADD] setup.py
    
    [IMP] - Add ace-widget with python mode
    
    [IMP] - Adapt contract_variable_quantity to contract new views
    
    [IMP] - refresh contract_variable_quantity.pot
    
    [REF] Contract: invoice in prepare_invoice_line is optional
    
    [IMP] - Skip zero should ignore lines with qty zero even for fixed qty
    
    [FIX] - Fix unit tests
    
    [FIX] fix unit tests
    
    [FIX] - fix check quantity = 0
    
    [REF] - Use method _get_quantity_to_invoice.

--- contract_variable_quantity/__manifest__.py
+++ contract_variable_quantity/__manifest__.py
@@ -4,17 +4,19 @@
 
 {
     'name': 'Variable quantity in contract recurrent invoicing',
-    'version': '11.0.1.3.0',
+    'version': '12.0.1.0.0',
     'category': 'Contract Management',
     'license': 'AGPL-3',
     'author': "Tecnativa,"
               "Odoo Community Association (OCA)",
     'website': 'https://www.tecnativa.com',
-    'depends': [
-        'contract',
-    ],
+    'depends': ['contract'],
     'data': [
         'security/ir.model.access.csv',
+        'views/abstract_contract_view.xml',
+        'views/contract_line_formula.xml',
+        'views/contract_line_view.xml',
+        'views/contract_template_view.xml',
         'views/contract_view.xml',
     ],
     'installable': True,
