d9ed4c8a3d204debaa4d4eb852b7318004a838e9
sbejaoui <souheil.bejaoui@acsone.eu>
2018-12-14 15:55:12 +0100

    [IMP] - show related sale orders in contract form

--- product_contract/models/__init__.py
+++ product_contract/models/__init__.py
@@ -2,6 +2,7 @@
 # Copyright 2017 LasLabs Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+from . import contract
 from . import contract_line
 from . import product_template
 from . import sale_order
