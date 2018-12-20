8bb027d58805eabb4aab7ae464cd23553f076371
Thomas Binsfeld <thomas.binsfeld@acsone.eu>
2018-12-20 16:06:52 +0100

    [MIG] Contract Mandate to 12.0

--- contract_mandate/__manifest__.py
+++ contract_mandate/__manifest__.py
@@ -1,14 +1,13 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Carlos Dauden - Tecnativa <carlos.dauden@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Contract Mandate',
     'summary': 'Mandate in contracts and their invoices',
-    'version': '10.0.1.0.0',
-    'author': 'Tecnativa, '
-              'Odoo Community Association (OCA)',
-    'website': 'http://www.tecnativa.com',
+    'version': '12.0.1.0.0',
+    'author': 'Odoo Community Association (OCA), '
+              'Tecnativa',
+    'website': 'https://github.com/OCA/contract',
     'depends': [
         'contract_payment_mode',
         'account_banking_mandate',
