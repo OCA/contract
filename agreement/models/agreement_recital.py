976aabde1d14345b35dba943a0d92db1fb08a674
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-01-08 18:04:26 -0600

    [IMP] Add dynamic field editor

--- agreement/models/agreement_recital.py
+++ agreement/models/agreement_recital.py
@@ -27,6 +27,43 @@ class AgreementRecital(models.Model):
         help="If unchecked, it will allow you to hide this recital without "
              "removing it.")
 
+    # Dynamic field editor
+    field_id = fields.Many2one(
+        'ir.model.fields', string="Field",
+        help="""Select target field from the related document model. If it is a
+         relationship field you will be able to select a target field at the
+         destination of the relationship.""")
+    sub_object_id = fields.Many2one(
+        'ir.model', string="Sub-model",
+        help="""When a relationship field is selected as first field, this
+         field shows the document model the relationship goes to.""")
+    sub_model_object_field_id = fields.Many2one(
+        'ir.model.fields', string="Sub-field",
+        help="""When a relationship field is selected as first field, this
+         field lets you select the target field within the destination document
+          model (sub-model).""")
+    default_value = fields.Char(
+        string="Default Value",
+        help="Optional value to use if the target field is empty.")
+    copyvalue = fields.Char(
+        string="Placeholder Expression",
+        help="""Final placeholder expression, to be copy-pasted in the desired
+         template field.""")
+
+    @api.onchange('field_id')
+    def onchange_sub_object_id(self):
+        if self.field_id:
+            self.sub_object_id = self.env['ir.model'].search(
+                [('model', '=', self.field_id.relation)])[0]
+
+    @api.onchange('sub_model_object_field_id', 'default_value')
+    def onchange_copyvalue(self):
+        if self.sub_model_object_field_id or self.default_value:
+            self.copyvalue = "${object.%s.%s or %s}" % \
+                             (self.field_id.name,
+                              self.sub_model_object_field_id.name,
+                              self.default_value or '\'\'')
+
     # compute the dynamic content for mako expression
     @api.multi
     def _compute_dynamic_content(self):
