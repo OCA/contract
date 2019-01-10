0f76c80372fff59f50384ce5705afca78a82e285
sbejaoui <souheil.bejaoui@acsone.eu>
2019-01-10 17:03:30 +0100

    [IMP] - Add unit test

--- product_contract/models/contract_line.py
+++ product_contract/models/contract_line.py
@@ -23,3 +23,23 @@ class AccountAnalyticInvoiceLine(models.Model):
         if self.sale_order_line_id and res:
             res['sale_line_ids'] = [(6, 0, [self.sale_order_line_id.id])]
         return res
+
+    @api.onchange('product_id')
+    def _onchange_product_id_recurring_info(self):
+        for rec in self:
+            rec.date_start = fields.Date.today()
+            if rec.product_id.is_contract:
+                rec.recurring_rule_type = rec.product_id.recurring_rule_type
+                rec.recurring_invoicing_type = (
+                    rec.product_id.recurring_invoicing_type
+                )
+                rec.recurring_interval = rec.product_id.recurring_interval
+                rec.is_auto_renew = rec.product_id.is_auto_renew
+                rec.auto_renew_interval = rec.product_id.auto_renew_interval
+                rec.auto_renew_rule_type = rec.product_id.auto_renew_rule_type
+                rec.termination_notice_interval = (
+                    rec.product_id.termination_notice_interval
+                )
+                rec.termination_notice_rule_type = (
+                    rec.product_id.termination_notice_rule_type
+                )
