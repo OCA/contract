92653ca14596420413d5c81c9ffe506924598481
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-01-11 17:59:59 -0600

    [IMP] agreement_sale

--- agreement_sale/models/__init__.py
+++ agreement_sale/models/__init__.py
@@ -1,6 +1,8 @@
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# Copyright (C) 2019 - TODAY, Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import (
     sale_order,
     agreement,
+    product,
 )
