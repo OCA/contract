7683e5a736896554ddf21e32b56a5f39dedda93d
Thomas Binsfeld <thomas.binsfeld@acsone.eu>
2019-01-21 16:46:14 +0100

    [IMP] Contract: matching invoice view depending on contract type
    [ADD] Contract: invoice partner

---
+++ contract/migrations/12.0.3.0.0/post-migration.py
@@ -0,0 +1,15 @@
+# Copyright 2019 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+import logging
+
+from odoo import SUPERUSER_ID, api
+
+_logger = logging.getLogger(__name__)
+
+
+def migrate(cr, version):
+    _logger.info("Populate invoicing partner field on contracts")
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    contracts = env["account.analytic.account"].search([])
+    contracts._inverse_partner_id()
