9bafbe9b39c276636aed1cc14fa0f8e253faafce
Nikul-Chaudhary <nikulchaudhary2112@gmail.com>
2019-01-24 00:27:27 +0530

    [IMP] Imp Code

--- contract_section/__manifest__.py
+++ contract_section/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Contracts Management - Add section to invoice lines',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Contract Management',
     'license': 'AGPL-3',
     'author': "Road-Support, "
