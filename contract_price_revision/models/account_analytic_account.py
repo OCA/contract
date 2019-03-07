8715f4e0db69baa50af9bcb0f4159ae63457b723
Pedro M. Baeza <pedro.baeza@gmail.com>
2019-03-07 13:39:52 +0100

    [IMP] contract_price_revision: Improve a bit performance

--- contract_price_revision/models/account_analytic_account.py
+++ contract_price_revision/models/account_analytic_account.py
@@ -9,7 +9,9 @@ class AccountAnalyticAccount(models.Model):
     @api.model
     def _prepare_invoice_line(self, line, invoice_id):
         line_obj = self.env['account.invoice.line']
-        invoice = self.env['account.invoice'].browse(invoice_id)
+        invoice = self.env['account.invoice'].browse(
+            invoice_id, prefetch=self._prefetch,
+        )
         # Line with automatic price are not taken into account
         if (line.date_start and invoice.date_invoice < line.date_start) or \
                 (line.date_end and invoice.date_invoice > line.date_end):
