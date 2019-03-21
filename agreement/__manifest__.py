41530e17ebc91084856a17737bdcf619cb9a1f63
Yves Goldberg <admin@ygol.com>
2019-03-21 11:40:56 +0100

    [FIX] flake8

--- agreement/__manifest__.py
+++ agreement/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # © 2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -7,7 +6,10 @@
     'summary': "Adds an agreement object",
     'version': '12.0.1.0.0',
     'category': 'Contract',
-    'author': "Akretion, Yves Goldberg (Ygol Internetwork), Odoo Community Association (OCA)",
+    'author': 
+        'Akretion, '
+        'Yves Goldberg (Ygol Internetwork),' 
+        'Odoo Community Association (OCA)',
     'website': 'https://github.com/oca/contract',
     'license': 'AGPL-3',
     'depends': ['base'],
