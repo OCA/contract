1685e7093a08e78d465aa9e0cf2779cf83607d51
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2019-03-28 17:52:39 +0530

    [MIG][WIP][12.0] agreement_stock

--- agreement_stock/__manifest__.py
+++ agreement_stock/__manifest__.py
@@ -4,13 +4,13 @@
 {
     'name': 'Agreement - Stock',
     'summary': 'Link picking to an agreement',
-    'version': '11.0.0.0.1',
+    'version': '12.0.1.0.0',
     'category': 'Contract',
     'author': 'Open Source Integrators, '
               'Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/contract',
     'depends': [
-        'agreement',
+        'agreement_serviceprofile',
         'stock',
     ],
     'data': [
