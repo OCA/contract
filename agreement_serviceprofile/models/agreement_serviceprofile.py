8fac774d0e286e388ebd67aa5c6abe3ee674766c
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2019-04-02 16:03:53 +0530

    [ADD][12.0] agreement_serviceprofile
    [UPD] README.rst
    
    [UPD] Update agreement_serviceprofile.pot
    
    Added translation using Weblate (French)
    
    Translated using Weblate (French)
    
    Currently translated at 100.0% (14 of 14 strings)
    
    Translation: contract-12.0/contract-12.0-agreement_serviceprofile
    Translate-URL: https://translation.odoo-community.org/projects/contract-12-0/contract-12-0-agreement_serviceprofile/fr/

---
+++ agreement_serviceprofile/models/agreement_serviceprofile.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2018 - TODAY, Pavlov Media
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+from odoo import fields, models
+
+
+class AgreementServiceProfile(models.Model):
+    _name = 'agreement.serviceprofile'
+    _description = 'Agreement Service Profiles'
+
+    name = fields.Char(string="Name", required=True)
+    agreement_id = fields.Many2one('agreement', string="Agreement",
+                                   ondelete="cascade")
+    active = fields.Boolean(
+        string="Active",
+        default=True,
+        help="If unchecked, it will allow you to hide this service profile"
+             " without removing it.")
