0786c67ebf7d53bd6a759c6a77fcda35eb13ba80
Florian da Costa <florian.dacosta@akretion.com>
2019-04-04 20:29:44 +0200

    Add module contract_invoice_start_end_dates

---
+++ contract_invoice_start_end_dates/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Akretion
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': 'Contract Invoice Start End Dates',
+    'version': '12.0.1.0.0',
+    'category': 'Contract Management',
+    'author': 'Akretion, '
+              'Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/contract',
+    'depends': [
+        'account_invoice_start_end_dates',
+        'contract',
+    ],
+    'data': [
+    ],
+    'license': 'AGPL-3',
+    'installable': True,
+}
---
+++ contract_invoice_start_end_dates/readme/CONTRIBUTORS.rst
@@ -0,0 +1,4 @@
+* `Akretion <https://www.akretion.com>`_:
+
+  * Alexis de Lattre <alexis.delattre@akretion.com>
+  * Florian da Costa <florian.dacosta@akretion.com>
