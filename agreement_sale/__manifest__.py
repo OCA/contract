5dd1433dcb81ae3d9dc4aa131b5d88b8b0d2d3db
David Beal <david.beal@akretion.com>
2019-04-05 18:11:30 +0200

    PORT agreement_sale

---
+++ agreement_sale/__manifest__.py
@@ -0,0 +1,28 @@
+# © 2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Agreement Sale',
+    'summary': "Agreement on sales",
+    'version': '12.0.1.0.0',
+    'category': 'Contract',
+    'author': 'Akretion, '
+              'Odoo Community Association (OCA)',
+    'website': 'https://github.com/oca/contract',
+    'license': 'AGPL-3',
+    'depends': [
+        'sale',
+        'agreement',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/agreement.xml',
+        ],
+    'demo': [],
+    'development_status': 'Beta',
+    'maintainers': [
+        'alexis-via',
+        'bealdav',
+    ],
+    'installable': True,
+}
