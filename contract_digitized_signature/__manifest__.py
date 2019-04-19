9dbf094d68db210bd54fd41066915dd3fea0fedf
cubells <vicent@vcubells.net>
2019-04-19 19:39:17 +0200

    [MIG] project_task_digitized_signature: Migration to 11.0

--- contract_digitized_signature/__manifest__.py
+++ contract_digitized_signature/__manifest__.py
@@ -1,13 +1,12 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Tecnativa - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Contract Digitized Signature",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "author": "Tecnativa, "
               "Odoo Community Association (OCA)",
-    "website": "https://www.tecnativa.com",
+    "website": "https://github.com/OCA/contract",
     "category": "Contract Management",
     "license": "AGPL-3",
     "depends": [
