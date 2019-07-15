63dc0c2e09c8feb9e56d839dfe8cbf49c38a4ed7
sbejaoui <souheil.bejaoui@acsone.eu>
2019-07-15 19:28:04 +0200

    [IMP] - Make one migration from 12.0.1 to 12.0.4
    [RMV] - Remove empty file
    
    [IMP] - Update no_update cron after migration
    
    [IMP] - move contract template recurrence info to line level
    
    [FIX] - Fix contract line model description
    
    [IMP] - Link contracts to analytic accounts
    
    [FIX] - Fix pylint
    
    [IMP] - Move chatter and attachments from analytic account to contract
    
    [IMP] - Move account_analytic_id to contract line level
    
    [IMP] - Improve version check in migration script
    
    [IMP] - Move contracts followers from analytic accounts
    
    [ADD] - Add mail.activity.mixin to contract.contract model
    
    remove data drop from migration scripts
    
    [12.0][FIX] - Fix _init_last_date_invoiced
    
    fix flake8
    
    [ADD] - Update contributors list

--- contract/migrations/12.0.4.0.0/pre-migration.py
+++ contract/migrations/12.0.4.0.0/pre-migration.py
@@ -12,20 +12,17 @@ _logger = logging.getLogger(__name__)
 def migrate(env, version):
     _logger.info(">> Pre-Migration 12.0.4.0.0")
     cr = env.cr
-    openupgrade.logged_query(
-        cr,
-        """
-        DROP TABLE IF EXISTS account_analytic_invoice_line_wizard
-        """
-    )
+
     models_to_rename = [
         # Contract Line Wizard
         ('account.analytic.invoice.line.wizard', 'contract.line.wizard'),
         # Abstract Contract
         ('account.abstract.analytic.contract', 'contract.abstract.contract'),
         # Abstract Contract Line
-        ('account.abstract.analytic.contract.line',
-         'contract.abstract.contract.line'),
+        (
+            'account.abstract.analytic.contract.line',
+            'contract.abstract.contract.line',
+        ),
         # Contract Line
         ('account.analytic.invoice.line', 'contract.line'),
         # Contract Template
@@ -40,20 +37,34 @@ def migrate(env, version):
         ('account_analytic_contract_line', 'contract_template_line'),
     ]
     xmlids_to_rename = [
-        ('contract.account_analytic_cron_for_invoice',
-         'contract.contract_cron_for_invoice'),
-        ('contract.account_analytic_contract_manager',
-         'contract.contract_template_manager'),
-        ('contract.account_analytic_contract_user',
-         'contract.contract_template_user'),
-        ('contract.account_analytic_invoice_line_manager',
-         'contract.contract_line_manager'),
-        ('contract.account_analytic_invoice_line_user',
-         'contract.contract_line_user'),
-        ('contract.account_analytic_contract_line_manager',
-         'contract.contract_template_line_manager'),
-        ('contract.account_analytic_contract_line_user',
-         'contract.contract_template_line_user'),
+        (
+            'contract.account_analytic_cron_for_invoice',
+            'contract.contract_cron_for_invoice',
+        ),
+        (
+            'contract.account_analytic_contract_manager',
+            'contract.contract_template_manager',
+        ),
+        (
+            'contract.account_analytic_contract_user',
+            'contract.contract_template_user',
+        ),
+        (
+            'contract.account_analytic_invoice_line_manager',
+            'contract.contract_line_manager',
+        ),
+        (
+            'contract.account_analytic_invoice_line_user',
+            'contract.contract_line_user',
+        ),
+        (
+            'contract.account_analytic_contract_line_manager',
+            'contract.contract_template_line_manager',
+        ),
+        (
+            'contract.account_analytic_contract_line_user',
+            'contract.contract_template_line_user',
+        ),
     ]
     openupgrade.rename_models(cr, models_to_rename)
     openupgrade.rename_tables(cr, tables_to_rename)
@@ -65,18 +76,38 @@ def migrate(env, version):
         """
         ALTER TABLE account_invoice_line
         ADD COLUMN contract_line_id_tmp INTEGER
-        """
+        """,
     )
-    openupgrade.logged_query(
-        cr,
-        """
-        UPDATE account_invoice_line
-        SET contract_line_id_tmp = contract_line_id
-        """
-    )
-    openupgrade.logged_query(
-        cr,
-        """
-        UPDATE account_invoice_line SET contract_line_id = NULL
-        """
-    )
+    if openupgrade.column_exists(
+        cr, 'account_invoice_line', 'contract_line_id'
+    ):
+        openupgrade.logged_query(
+            cr,
+            """
+            UPDATE account_invoice_line
+            SET contract_line_id_tmp = contract_line_id
+            """,
+        )
+        openupgrade.logged_query(
+            cr,
+            """
+            UPDATE account_invoice_line SET contract_line_id = NULL
+            """,
+        )
+    if not openupgrade.column_exists(
+        cr, 'account_invoice', 'old_contract_id'
+    ):
+        openupgrade.logged_query(
+            cr,
+            """
+            ALTER TABLE account_invoice
+            ADD COLUMN old_contract_id_tmp INTEGER
+            """,
+        )
+        openupgrade.logged_query(
+            cr,
+            """
+            UPDATE account_invoice
+            SET old_contract_id_tmp = contract_id
+            """,
+        )
