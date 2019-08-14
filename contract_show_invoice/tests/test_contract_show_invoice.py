f9365e703dda1b24dc4961acf3c7f6494cc2eac8
sergiocorato <sergiocorato@gmail.com>
2019-08-14 12:45:53 +0200

    [10.0][FIX] tests not working

--- contract_show_invoice/tests/test_contract_show_invoice.py
+++ contract_show_invoice/tests/test_contract_show_invoice.py
@@ -16,17 +16,40 @@ class TestContractShowInvoice(TransactionCase):
              'type': 'out_invoice',
              })
         self.invoice._onchange_partner_id()
+        self.a_sale = self.env['account.account'].search([
+            ('user_type_id', '=',
+            self.env.ref('account.data_account_type_revenue').id)
+        ], limit=1)
         self.invoice_line = self.env['account.invoice.line'].create(
             {'product_id': self.env.ref('product.product_product_2').id,
              'quantity': 1.0,
              'invoice_id': self.invoice.id,
              'account_analytic_id': self.analytic_account.id,
+             'account_id': self.a_sale.id,
+             'price_unit': 50.0,
+             'name': 'Invoice line with analytic account',
              })
         self.invoice_line._onchange_product_id()
 
     def test_contract_show_invoice(self):
-        self.assertEqual(len(self.analytic_account.analytic_account_ids), 1)
+        self.assertEqual(len(self.invoice.analytic_account_ids), 1)
 
     def test_contract_total_invoiced(self):
         self.assertEqual(self.invoice.amount_total,
                          self.analytic_account.total_invoiced)
+
+    def test_invoice_two_line(self):
+        self.invoice_line1 = self.env['account.invoice.line'].create(
+            {'product_id': self.env.ref('product.product_product_2').id,
+             'quantity': 2.0,
+             'invoice_id': self.invoice.id,
+             'account_analytic_id': False,
+             'account_id': self.a_sale.id,
+             'price_unit': 100.0,
+             'name': 'Invoice line without analytic account',
+             })
+        self.assertFalse(self.invoice_line1.account_analytic_id)
+        self.assertNotEqual(
+            self.analytic_account.total_invoiced, self.invoice.amount_total,
+            msg="Amount invoiced is from total of invoice, even if only 1 line"
+                " has analytic account")
