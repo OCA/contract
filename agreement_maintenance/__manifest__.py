4494b623fec7ecfb8400f50588f14b17e6c3c3a6
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-08-31 14:40:02 -0500

    [IMP] agreement_maintenance: Link Equipments and Service Profiles

--- agreement_maintenance/__manifest__.py
+++ agreement_maintenance/__manifest__.py
@@ -1,4 +1,5 @@
-# Copyright (C) 2018 - TODAY, Pavlov Media
+# Copyright (C) 2018 Pavlov Media
+# Copyright (C) 2019 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
@@ -10,14 +11,16 @@
     'website': 'https://github.com/OCA/contract',
     'category': 'Maintenance',
     'license': 'AGPL-3',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.1.0',
     'depends': [
         'maintenance',
         'agreement_serviceprofile',
     ],
     'data': [
         'views/agreement_view.xml',
+        'views/agreement_serviceprofile_view.xml',
         'views/maintenance_request_view.xml',
+        'views/maintenance_equipment_view.xml',
     ],
     'development_status': 'Beta',
     'maintainers': ['max3903'],
