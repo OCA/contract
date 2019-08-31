a8314fbd2caa6900aee7127aa75200c0acc76e32
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-08-31 14:47:09 -0500

    [IMP] agreement_serviceprofile

--- agreement_serviceprofile/models/agreement_serviceprofile.py
+++ agreement_serviceprofile/models/agreement_serviceprofile.py
@@ -26,7 +26,10 @@ class AgreementServiceProfile(models.Model):
 
     notes = fields.Text(string="Notes")
     product_id = fields.Many2one('product.template', 'Service',
-                                 domain="[('type', '=', 'service')]")
+                                 domain="[('type', '=', 'service')]",
+                                 required=True)
+    partner_id = fields.Many2one(related='agreement_id.partner_id',
+                                 string='Partner')
 
     # Used for Kanban grouped_by view
     @api.model
