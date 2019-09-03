e99584c3e787c583b8e8792b8675d361feb91129
黎伟杰 <674416404@qq.com>
2019-09-03 11:36:40 +0000

    [IMP] move some notions from agreement_legal to agreement
    we move the is_template field definition and the agreement.type model from
    the agreement_legal module to the agreement module.
    
    The fields are not displayed by default, unless the feature is enabled through a
    technical feature group, this is configurable in the agreement_sale module (because agreement
    in itself has no UI, and agreement_legal enables the feature by default)

--- agreement/models/agreement.py
+++ agreement/models/agreement.py
@@ -12,11 +12,23 @@ class Agreement(models.Model):
     code = fields.Char(required=True, copy=False)
     name = fields.Char(required=True)
     partner_id = fields.Many2one(
-        'res.partner', string='Partner', ondelete='restrict', required=True,
+        'res.partner', string='Partner', ondelete='restrict',
         domain=[('parent_id', '=', False)])
     company_id = fields.Many2one(
         'res.company', string='Company',
         default=lambda self: self.env['res.company']._company_default_get())
+    is_template = fields.Boolean(
+        string="Is a Template?",
+        default=False,
+        copy=False,
+        help="Set if the agreement is a template. "
+        "Template agreements don't require a partner."
+    )
+    agreement_type_id = fields.Many2one(
+        'agreement.type',
+        string="Agreement Type",
+        help="Select the type of agreement",
+    )
     active = fields.Boolean(default=True)
     signature_date = fields.Date()
     start_date = fields.Date()
