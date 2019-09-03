39e6ef673056ec8a5aa79a4276f03f40b65868aa
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-09-03 11:15:09 -0500

    Update agreement_serviceprofile.py

--- agreement_serviceprofile/models/agreement_serviceprofile.py
+++ agreement_serviceprofile/models/agreement_serviceprofile.py
@@ -25,7 +25,7 @@ class AgreementServiceProfile(models.Model):
                             " without removing it.")
 
     notes = fields.Text(string="Notes")
-    product_id = fields.Many2one('product.template', 'Service',
+    product_id = fields.Many2one('product.template', 'Service Product',
                                  domain="[('type', '=', 'service')]",
                                  required=True)
     partner_id = fields.Many2one(related='agreement_id.partner_id',
