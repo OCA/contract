173be4c56cd5c9998eb6826b1c71c13d48541d5a
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-09-05 12:17:51 -0500

    [FIX] agreement_serviceprofile: Github Issue #370
    [UPD] Update agreement_serviceprofile.pot
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: contract-12.0/contract-12.0-agreement_serviceprofile
    Translate-URL: https://translation.odoo-community.org/projects/contract-12-0/contract-12-0-agreement_serviceprofile/
    
    Translated using Weblate (French)
    
    Currently translated at 100.0% (27 of 27 strings)
    
    Translation: contract-12.0/contract-12.0-agreement_serviceprofile
    Translate-URL: https://translation.odoo-community.org/projects/contract-12-0/contract-12-0-agreement_serviceprofile/fr/
    
    Translated using Weblate (Chinese (Simplified))
    
    Currently translated at 100.0% (27 of 27 strings)
    
    Translation: contract-12.0/contract-12.0-agreement_serviceprofile
    Translate-URL: https://translation.odoo-community.org/projects/contract-12-0/contract-12-0-agreement_serviceprofile/zh_CN/

--- agreement_serviceprofile/models/agreement_serviceprofile.py
+++ agreement_serviceprofile/models/agreement_serviceprofile.py
@@ -26,7 +26,8 @@ class AgreementServiceProfile(models.Model):
 
     notes = fields.Text(string="Notes")
     product_id = fields.Many2one('product.template', 'Service Product',
-                                 domain="[('type', '=', 'service')]",
+                                 domain="[('is_serviceprofile', '=', True), "
+                                        "('type', '=', 'service')]",
                                  required=True)
     partner_id = fields.Many2one(related='agreement_id.partner_id',
                                  string='Partner')
