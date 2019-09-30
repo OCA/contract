02aae61126fd5c5f5b1138e6ff5cacf5080de21d
sbejaoui <souheil.bejaoui@acsone.eu>
2019-09-30 11:23:35 +0200

    [12.0][REF] - Contract Variable quantity: split from analytic accoun

--- contract_variable_quantity/models/contract_line.py
+++ contract_variable_quantity/models/contract_line.py
@@ -9,7 +9,7 @@ from odoo.tools.safe_eval import safe_eval
 
 
 class AccountAnalyticInvoiceLine(models.Model):
-    _inherit = 'account.analytic.invoice.line'
+    _inherit = 'contract.line'
 
     @api.multi
     def _get_quantity_to_invoice(
