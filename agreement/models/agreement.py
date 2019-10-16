61fab03a22e4e0b59f180105b0fafc513ae63f7d
Iryna Vyshnevska <i.vyshnevska@mobilunity.com>
2019-10-16 17:43:39 +0300

    [13.0][MIG] agreement: migrate + black

--- agreement/models/agreement.py
+++ agreement/models/agreement.py
@@ -2,32 +2,35 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class Agreement(models.Model):
-    _name = 'agreement'
-    _description = 'Agreement'
+    _name = "agreement"
+    _description = "Agreement"
 
     code = fields.Char(required=True, copy=False)
     name = fields.Char(required=True)
     partner_id = fields.Many2one(
-        'res.partner', string='Partner', ondelete='restrict',
-        domain=[('parent_id', '=', False)])
+        "res.partner",
+        string="Partner",
+        ondelete="restrict",
+        domain=[("parent_id", "=", False)],
+    )
     company_id = fields.Many2one(
-        'res.company', string='Company',
-        default=lambda self: self.env['res.company']._company_default_get())
+        "res.company",
+        string="Company",
+        default=lambda self: self.env["res.company"]._company_default_get(),
+    )
     is_template = fields.Boolean(
         string="Is a Template?",
         default=False,
         copy=False,
         help="Set if the agreement is a template. "
-        "Template agreements don't require a partner."
+        "Template agreements don't require a partner.",
     )
     agreement_type_id = fields.Many2one(
-        'agreement.type',
-        string="Agreement Type",
-        help="Select the type of agreement",
+        "agreement.type", string="Agreement Type", help="Select the type of agreement"
     )
     active = fields.Boolean(default=True)
     signature_date = fields.Date()
@@ -39,12 +42,14 @@ class Agreement(models.Model):
         for agr in self:
             name = agr.name
             if agr.code:
-                name = '[%s] %s' % (agr.code, agr.name)
+                name = "[{}] {}".format(agr.code, agr.name)
             res.append((agr.id, name))
         return res
 
-    _sql_constraints = [(
-        'code_partner_company_unique',
-        'unique(code, partner_id, company_id)',
-        'This agreement code already exists for this partner!'
-        )]
+    _sql_constraints = [
+        (
+            "code_partner_company_unique",
+            "unique(code, partner_id, company_id)",
+            "This agreement code already exists for this partner!",
+        )
+    ]
