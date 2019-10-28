5418e3569c134376c12fdbf329c768ef7f78ffcc
sbejaoui <souheil.bejaoui@acsone.eu>
2019-10-28 17:52:30 +0100

    [12.0][FIX] - contract company must be the same as the sale order
    When creating a contract from a sale order the company must be the sale order
    company and not the user company

--- contract/__manifest__.py
+++ contract/__manifest__.py
@@ -9,7 +9,7 @@
 
 {
     'name': 'Recurring - Contracts Management',
-    'version': '12.0.4.2.0',
+    'version': '12.0.4.2.4',
     'category': 'Contract Management',
     'license': 'AGPL-3',
     'author': "OpenERP SA, "
