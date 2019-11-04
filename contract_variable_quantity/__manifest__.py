3606d696c12d4d72070829f38eb0ea21a47fb7ae
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-11-04 11:07:38 +0100

    [FIX] contract_variable_quantity: Allow to propagate variable qty fields from template
    **Steps to reproduce the problem**
    
    - Define a contract template with one line with variable quantity and formula.
    - Use that template in a new contract.
    - Variable quantity type and formula are not transferred from template to contract.
    
    This is due to the lack of that fields in the tree view that prevents web client to fetch
    and store them.
    
    We solve it adding those fields to the contract line tree view, also providing some
    visibility logic for not showing the corresponding fields according the type.
    
    No regression tests is provided, as this is only reproducible with web client, not through
    code.

--- contract_variable_quantity/__manifest__.py
+++ contract_variable_quantity/__manifest__.py
@@ -1,4 +1,4 @@
-# Copyright 2016-2018 Tecnativa - Pedro M. Baeza
+# Copyright 2016-2019 Tecnativa - Pedro M. Baeza
 # Copyright 2018 Tecnativa - Carlos Dauden
 # Copyright 2019 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
@@ -16,6 +16,7 @@
         'security/ir.model.access.csv',
         'views/abstract_contract_line.xml',
         'views/contract_line_formula.xml',
+        'views/contract_line_views.xml',
         'views/contract_template.xml',
         'views/contract.xml',
     ],
