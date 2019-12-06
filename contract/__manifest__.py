63a8d4fc9f2ccb3563777c5480c28f66d30e143f
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2019-12-06 14:12:18 +0100

    [REF+FIX+IMP] contract: Several refactorings + fixes + imps
    - REF: Refactor _update_recurring_next_date
      Reuse the logic that is now fully located in _get_recurring_next_date.
    - REF: re-add _compute_first_recurring_next_date
      for backward compatibility
    - FIX: add missing dependency in computed field
    - REF: remove one monthlylastday special case
      get_relative_delta now works the same for all recurring rules.
      Move the special case handling to _init_last_date_invoiced
      which is used only for migration.
    - IMP: support pre-paid for monthlylastday
      monthlylastday is (almost) not a special case anymore \o/.
      montlylastday is simply a montly period where the
      periods are aligned on month boundaries.
      The last bit of special casing is that postpaid generates
      invoice the day after the last dasy of the period, except
      for monthlylastday where the invoice is generated on the
      last day of the period. This last exception will disappear
      when we put the offset under user control.
      This is a breaking change because the post-paid/pre-paid
      mode becomes relevant for monthlylastday invoicing.
      The field becomes visible in the UI. Code that generate
      monthlylastday contract lines must now correctly set
      the pre-paid/post-paid mode too. Some tests have had
      to be adapted to reflect that.
    - REF: make recurring_invoicing_offset a computed field
      In preparation to making it user modifiable.
    - REF: make get_next_period_date_end public
      Make it public because it is the core logic of the module.
      Also, clarify that recurring_invoicing_type
      and recurring_invoicing_offset are needed only when
      we want the next period to be computed from a
      user chosen next invoice date.
    - REF: rename _get_recurring_next_date as get_next_invoice_date
      It is easier to understand. Also make it public.

--- contract/__manifest__.py
+++ contract/__manifest__.py
@@ -4,7 +4,7 @@
 # Copyright 2016-2018 Tecnativa - Carlos Dauden
 # Copyright 2017 Tecnativa - Vicent Cubells
 # Copyright 2016-2017 LasLabs Inc.
-# Copyright 2018 ACSONE SA/NV
+# Copyright 2018-2019 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
