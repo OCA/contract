17462d96fd49933c0f1bc78117b9cb3237c59a60
sbejaoui <souheil.bejaoui@acsone.eu>
2019-12-21 14:05:15 +0100

    [IMP+FIX] contract: Several things
    - Add failing test for next invoice date before the last date invoiced
    - raise an error when next invoice date before the last date invoiced
    - Add note field to contract
    - add new option: create_new_line_at_contract_line_renew
      Add a company config option to decide whether to create or to extend contract
      line at renew action
    - extend contract line at renewal
    - improve code: unify methods argument _renew_create_line and _renew_extend_line

---
+++ contract/models/res_company.py
@@ -0,0 +1,17 @@
+# Copyright 2019 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+
+    _inherit = 'res.company'
+
+    create_new_line_at_contract_line_renew = fields.Boolean(
+        string="Create New Line At Contract Line Renew",
+        help="If checked, a new line will be generated at contract line renew "
+        "and linked to the original one as successor. The default "
+        "behavior is to extend the end date of the contract by a new "
+        "subscription period",
+    )
