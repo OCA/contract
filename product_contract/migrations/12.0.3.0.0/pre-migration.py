2dd2d88bc669adadf8f67513a5e0df3447165305
sergiocorato <sergiocorato@gmail.com>
2020-03-24 10:44:48 +0100

    [FIX] migration from 11.0
    Update product_contract/migrations/12.0.3.0.0/pre-migration.py
    
    Co-Authored-By: Bejaoui Souheil <souheil_bejaoui@hotmail.fr>
    
    Update product_contract/migrations/12.0.3.0.0/pre-migration.py
    
    Co-Authored-By: Bejaoui Souheil <souheil_bejaoui@hotmail.fr>
    
    [FIX]

--- product_contract/migrations/12.0.3.0.0/pre-migration.py
+++ product_contract/migrations/12.0.3.0.0/pre-migration.py
@@ -1,10 +1,14 @@
+from odoo.tools import parse_version
+
+
 def migrate(cr, version):
-    # pre-paid/post-paid becomes significant for monthlylastday too,
-    # make sure it has the value that was implied for previous versions.
-    cr.execute(
-        """\
-            UPDATE product_template
-            SET recurring_invoicing_type = 'post-paid'
-            WHERE recurring_rule_type = 'monthlylastday'
-        """
-    )
+    if parse_version(version) == parse_version('12.0.2.0.0'):
+        # pre-paid/post-paid becomes significant for monthlylastday too,
+        # make sure it has the value that was implied for previous versions.
+        cr.execute(
+            """\
+                UPDATE product_template
+                SET recurring_invoicing_type = 'post-paid'
+                WHERE recurring_rule_type = 'monthlylastday'
+            """
+        )
