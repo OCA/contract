85235d575cb480061ea924bedf46e6f9142519bf
ahenriquez <ahenriquez@eficent.com>
2020-03-30 10:27:14 +0200

    [FIX]product_contract pre migration

--- product_contract/migrations/12.0.3.0.0/pre-migration.py
+++ product_contract/migrations/12.0.3.0.0/pre-migration.py
@@ -1,10 +1,13 @@
+from openupgradelib import openupgrade
+
 def migrate(cr, version):
     # pre-paid/post-paid becomes significant for monthlylastday too,
     # make sure it has the value that was implied for previous versions.
-    cr.execute(
-        """\
-            UPDATE product_template
-            SET recurring_invoicing_type = 'post-paid'
-            WHERE recurring_rule_type = 'monthlylastday'
-        """
-    )
+    if openupgrade.column_exists(cr, 'product_template', 'recurring_invoicing_type'):
+        cr.execute(
+            """\
+                UPDATE product_template
+                SET recurring_invoicing_type = 'post-paid'
+                WHERE recurring_rule_type = 'monthlylastday'
+            """
+        )
