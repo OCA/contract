e68dab686d14a5337420308c7d97f63a4e92926c
Bejaoui Souheil <souheil_bejaoui@hotmail.fr>
2020-04-02 17:14:04 +0200

    [IMP] - Place id attribute before model
    Co-Authored-By: Carlos Dauden <carlos.dauden@tecnativa.com>

--- contract/__manifest__.py
+++ contract/__manifest__.py
@@ -9,7 +9,7 @@
 
 {
     'name': 'Recurring - Contracts Management',
-    'version': '12.0.6.0.3',
+    'version': '12.0.6.0.4',
     'category': 'Contract Management',
     'license': 'AGPL-3',
     'author': "OpenERP SA, "
