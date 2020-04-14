4dcb5af058bb1b1c63c335c2906cf0c79909a8a0
sbejaoui <souheil.bejaoui@acsone.eu>
2020-04-14 11:25:09 +0200

    [IMP] - Contract: add new recurring_rule_type quarterly and semesterly

--- contract/__manifest__.py
+++ contract/__manifest__.py
@@ -9,7 +9,7 @@
 
 {
     'name': 'Recurring - Contracts Management',
-    'version': '12.0.7.1.0',
+    'version': '12.0.7.2.0',
     'category': 'Contract Management',
     'license': 'AGPL-3',
     'author': "OpenERP SA, "
