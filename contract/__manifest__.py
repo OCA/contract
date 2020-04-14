942567cbe3bf474b4c44e015e26f69bdcfa26908
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-04-14 13:14:10 +0200

    [FIX+IMP] contract: improve test coverage with new sections + fix search method

--- contract/__manifest__.py
+++ contract/__manifest__.py
@@ -9,7 +9,7 @@
 
 {
     'name': 'Recurring - Contracts Management',
-    'version': '12.0.6.0.4',
+    'version': '12.0.7.0.0',
     'category': 'Contract Management',
     'license': 'AGPL-3',
     'author': "OpenERP SA, "
