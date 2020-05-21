d5418442b4515329b33f76c143a3a9ac6f704903
Alexis de Lattre <alexis.delattre@akretion.com>
2020-05-21 16:55:05 +0200

    Re-introduce module agreement_account
    Update agreement and agreement_sale to restore the behavior of v10

--- agreement/models/agreement.py
+++ agreement/models/agreement.py
@@ -10,11 +10,11 @@ class Agreement(models.Model):
     _description = 'Agreement'
     _inherit = ["mail.thread", "mail.activity.mixin"]
 
-    code = fields.Char(required=True)
-    name = fields.Char(required=True)
+    code = fields.Char(required=True, track_visibility='onchange')
+    name = fields.Char(required=True, track_visibility='onchange')
     partner_id = fields.Many2one(
         'res.partner', string='Partner', ondelete='restrict',
-        domain=[('parent_id', '=', False)])
+        domain=[('parent_id', '=', False)], track_visibility='onchange')
     company_id = fields.Many2one(
         'res.company', string='Company',
         default=lambda self: self.env['res.company']._company_default_get())
@@ -30,10 +30,25 @@ class Agreement(models.Model):
         string="Agreement Type",
         help="Select the type of agreement",
     )
+    domain = fields.Selection(
+        '_domain_selection', string='Domain', default='sale',
+        track_visibility='onchange')
     active = fields.Boolean(default=True)
-    signature_date = fields.Date()
-    start_date = fields.Date()
-    end_date = fields.Date()
+    signature_date = fields.Date(track_visibility='onchange')
+    start_date = fields.Date(track_visibility='onchange')
+    end_date = fields.Date(track_visibility='onchange')
+
+    @api.model
+    def _domain_selection(self):
+        return [
+            ('sale', _('Sale')),
+            ('purchase', _('Purchase')),
+            ]
+
+    @api.onchange('agreement_type_id')
+    def agreement_type_change(self):
+        if self.agreement_type_id and self.agreement_type_id.domain:
+            self.domain = self.agreement_type_id.domain
 
     def name_get(self):
         res = []
