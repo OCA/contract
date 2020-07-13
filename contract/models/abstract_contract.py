92a4e7781efaf0e8f38174111ea9f8d3969d8905
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-07-13 18:28:03 +0200

    [IMP] : black, isort, prettier

--- contract/models/abstract_contract.py
+++ contract/models/abstract_contract.py
@@ -6,68 +6,61 @@
 # Copyright 2018 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, models, fields
+from odoo import api, fields, models
 
 
 class ContractAbstractContract(models.AbstractModel):
-    _name = 'contract.abstract.contract'
-    _description = 'Abstract Recurring Contract'
+    _name = "contract.abstract.contract"
+    _description = "Abstract Recurring Contract"
 
     # These fields will not be synced to the contract
-    NO_SYNC = ['name', 'partner_id', 'company_id']
+    NO_SYNC = ["name", "partner_id", "company_id"]
 
     name = fields.Char(required=True)
     # Needed for avoiding errors on several inherited behaviors
     partner_id = fields.Many2one(
         comodel_name="res.partner", string="Partner", index=True
     )
-    pricelist_id = fields.Many2one(
-        comodel_name='product.pricelist', string='Pricelist'
-    )
+    pricelist_id = fields.Many2one(comodel_name="product.pricelist", string="Pricelist")
     contract_type = fields.Selection(
-        selection=[('sale', 'Customer'), ('purchase', 'Supplier')],
-        default='sale',
+        selection=[("sale", "Customer"), ("purchase", "Supplier")],
+        default="sale",
         index=True,
     )
 
     journal_id = fields.Many2one(
-        'account.journal',
-        string='Journal',
+        "account.journal",
+        string="Journal",
         default=lambda s: s._default_journal(),
-        domain="[('type', '=', contract_type),"
-        "('company_id', '=', company_id)]",
+        domain="[('type', '=', contract_type)," "('company_id', '=', company_id)]",
         index=True,
     )
     company_id = fields.Many2one(
-        'res.company',
-        string='Company',
+        "res.company",
+        string="Company",
         required=True,
-        default=lambda self: self.env['res.company']._company_default_get(
-            self._name
-        ),
+        default=lambda self: self.env["res.company"]._company_default_get(self._name),
     )
 
-    @api.onchange('contract_type')
+    @api.onchange("contract_type")
     def _onchange_contract_type(self):
-        if self.contract_type == 'purchase':
-            self.contract_line_ids.filtered('automatic_price').update(
-                {'automatic_price': False}
+        if self.contract_type == "purchase":
+            self.contract_line_ids.filtered("automatic_price").update(
+                {"automatic_price": False}
             )
-        self.journal_id = self.env['account.journal'].search(
+        self.journal_id = self.env["account.journal"].search(
             [
-                ('type', '=', self.contract_type),
-                ('company_id', '=', self.company_id.id),
+                ("type", "=", self.contract_type),
+                ("company_id", "=", self.company_id.id),
             ],
             limit=1,
         )
 
     @api.model
     def _default_journal(self):
-        company_id = self.env.context.get(
-            'company_id', self.env.user.company_id.id
-        )
+        company_id = self.env.context.get("company_id", self.env.user.company_id.id)
         domain = [
-            ('type', '=', self.contract_type),
-            ('company_id', '=', company_id),
+            ("type", "=", self.contract_type),
+            ("company_id", "=", company_id),
         ]
-        return self.env['account.journal'].search(domain, limit=1)
+        return self.env["account.journal"].search(domain, limit=1)
