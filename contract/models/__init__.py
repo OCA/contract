1a31ec1ee771010de07997f4fb723c7a18e767c4
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-07-14 20:23:08 +0200

    [IMP+REF] contract: Allow to set recurrency at header level
    Big refactoring for allowing to define recurrency at header level for simplifying
    the use of the module for most of the cases where you don't need different
    recurrency at line level.

--- contract/models/__init__.py
+++ contract/models/__init__.py
@@ -1,5 +1,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from . import contract_recurrency_mixin  # should be first
 from . import abstract_contract
 from . import abstract_contract_line
 from . import contract_template
