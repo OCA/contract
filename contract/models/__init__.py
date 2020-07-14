953aa7e5df5f98b8eea5dbdc173e6f68d41104a8
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-07-14 12:10:42 +0200

    [MIG] contract: Migration to 13.0
    - Standard procedure
    - Adapt invoice creation to new account.move
    - Tests adapted

--- contract/models/__init__.py
+++ contract/models/__init__.py
@@ -6,11 +6,9 @@ from . import contract_template
 from . import contract
 from . import contract_template_line
 from . import contract_line
-from . import account_invoice
-from . import account_invoice_line
+from . import account_move
 from . import res_partner
 from . import contract_tag
 from . import res_company
 from . import res_config_settings
 from . import contract_terminate_reason
-from . import ir_ui_view
