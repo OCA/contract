b3389b7218f30ee214142a45e9b7ef8717f08c88
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2020-09-30 22:01:22 +0530

    [WIP][12.0][MIG] contract_sale_generation

--- contract_sale_generation/models/__init__.py
+++ contract_sale_generation/models/__init__.py
@@ -1,5 +1,6 @@
-# -*- coding: utf-8 -*-
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import account_analytic_contract
-from . import account_analytic_account
+from . import abstract_contract
+from . import contract
+from . import sale_order_line
+from . import contract_line
--- contract_sale_generation/models/abstract_contract.py
+++ contract_sale_generation/models/abstract_contract.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Pesol (<http://pesol.es>)
 # Copyright 2017 Angel Moya <angel.moya@pesol.es>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
@@ -6,15 +5,15 @@
 from odoo import fields, models
 
 
-class AccountAnalyticContract(models.Model):
-    _inherit = 'account.analytic.contract'
+class ContractAbstractContract(models.AbstractModel):
+    _inherit = 'contract.abstract.contract'
 
     type = fields.Selection(
+        [('invoice', 'Invoice'),
+         ('sale', 'Sale')],
         string='Type',
-        selection=[('invoice', 'Invoice'),
-                   ('sale', 'Sale')],
         default='invoice',
         required=True,
     )
     sale_autoconfirm = fields.Boolean(
-        string='Sale autoconfirm')
+        string='Sale Autoconfirm')
