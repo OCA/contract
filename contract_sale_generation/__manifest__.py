aa8bf8305449a5b3c7c0392cab75e68b8c5576d0
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2020-09-30 22:01:22 +0530

    [WIP][12.0][MIG] contract_sale_generation

--- contract_sale_generation/__manifest__.py
+++ contract_sale_generation/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Pesol (<http://pesol.es>)
 # Copyright 2017 Angel Moya <angel.moya@pesol.es>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
@@ -6,7 +5,7 @@
 
 {
     'name': 'Contracts Management - Recurring Sales',
-    'version': '10.0.3.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Contract Management',
     'license': 'AGPL-3',
     'author': "PESOL, "
@@ -14,10 +13,9 @@
     'website': 'https://github.com/oca/contract',
     'depends': ['contract', 'sale'],
     'data': [
-        'views/account_analytic_account_view.xml',
-        'views/account_analytic_contract_view.xml',
-        'views/sale_view.xml',
         'data/contract_cron.xml',
+        'views/contract.xml',
+        'views/contract_template.xml',
     ],
     'installable': True,
 }
---
+++ contract_sale_generation/readme/CONTRIBUTORS.rst
@@ -0,0 +1,3 @@
+* Angel Moya <angel.moya@pesol.es>
+* Florent THOMAS <florent.thomas@mind-and-go.com>
+* Serpent Consulting Services Pvt. Ltd. <support@serpentcs.com>
