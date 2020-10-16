21409aad8514eae175f08139556cd961a8fc354d
Víctor Martínez <victor.martinez@tecnativa.com>
2020-10-16 12:33:50 +0200

    [IMP] contract_sale: black, isort, prettier

--- contract_sale/__manifest__.py
+++ contract_sale/__manifest__.py
@@ -2,26 +2,22 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    'name': 'Contract from Sale',
-    'version': '12.0.3.0.0',
-    'category': 'Sales',
-    'author': 'Tecnativa, '
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/contract',
-    'depends': [
-        'sale',
-        'contract',
+    "name": "Contract from Sale",
+    "version": "12.0.3.0.0",
+    "category": "Sales",
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/contract",
+    "depends": ["sale", "contract"],
+    "data": [
+        "security/ir.model.access.csv",
+        "security/contract_security.xml",
+        "views/abstract_contract_line.xml",
+        "views/contract.xml",
+        "views/contract_line.xml",
+        "views/contract_template.xml",
+        "views/res_partner_view.xml",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'security/contract_security.xml',
-        'views/abstract_contract_line.xml',
-        'views/contract.xml',
-        'views/contract_line.xml',
-        'views/contract_template.xml',
-        'views/res_partner_view.xml',
-    ],
-    'license': 'AGPL-3',
-    'installable': True,
-    'auto_install': True,
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
 }
