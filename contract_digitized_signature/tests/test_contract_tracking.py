0fac96690c857513d9bc9dbc056a4c373726d1fb
Carms Ng <carms.ng@technolibre.ca>
2020-10-16 12:08:18 -0400

    [FIX] added missing column partner_id to test

--- contract_digitized_signature/tests/test_contract_tracking.py
+++ contract_digitized_signature/tests/test_contract_tracking.py
@@ -9,14 +9,18 @@ class TestContractSignatureTracking(common.SavepointCase):
     def setUpClass(cls):
         super(TestContractSignatureTracking, cls).setUpClass()
         # Simple 1x1 transparent base64 encoded GIF
-        cls.image = 'R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=='
+        cls.image = "R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
 
     def test_contract_signature_tracking(self):
-        self.contract = self.env['contract.contract'].create({
-            'name': 'Test Contract',
-            'customer_signature': self.image,
-        })
-        message = self.env['mail.message'].search([
-            ('res_id', '=', self.contract.id)
-        ], order='id desc', limit=1)
-        self.assertIn('Signature has been created.', message.body)
+        partner = self.env["res.partner"].create({"name": "test partner"})
+        self.contract = self.env["contract.contract"].create(
+            {
+                "name": "Test Contract",
+                "customer_signature": self.image,
+                "partner_id": partner.id,
+            }
+        )
+        message = self.env["mail.message"].search(
+            [("res_id", "=", self.contract.id)], order="id desc", limit=1
+        )
+        self.assertIn("Signature has been created.", message.body)
