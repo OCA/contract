994017e4d4668fb712077bacac7a587ff2f4a260
sbejaoui <souheil.bejaoui@acsone.eu>
2020-10-16 12:42:34 +0200

    [ADD] - add contract_queue_job and contract_invoice_auto_validate

---
+++ contract_queue_job/models/contract_contract.py
@@ -0,0 +1,22 @@
+# Copyright 2020 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+from odoo.addons.queue_job.job import job
+
+QUEUE_CHANNEL = "root.CONTRACT_INVOICE"
+
+
+class ContractContract(models.Model):
+
+    _inherit = "contract.contract"
+
+    @api.multi
+    @job(default_channel=QUEUE_CHANNEL)
+    def _recurring_create_invoice(self, date_ref=False):
+        res = self.env["account.invoice"]
+        if len(self) > 1:
+            for rec in self:
+                rec.with_delay()._recurring_create_invoice(date_ref=date_ref)
+            return res
+        return super()._recurring_create_invoice(date_ref=date_ref)
---
+++ contract_queue_job/wizards/contract_manually_create_invoice.py
@@ -0,0 +1,15 @@
+# Copyright 2020 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+
+
+class ContractManuallyCreateInvoice(models.TransientModel):
+
+    _inherit = "contract.manually.create.invoice"
+
+    @api.multi
+    def create_invoice(self):
+        self.ensure_one()
+        self.contract_to_invoice_ids._recurring_create_invoice()
+        return {}
