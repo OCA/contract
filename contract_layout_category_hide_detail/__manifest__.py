f6101cddfaad7de3da466cf7cb099377c3cb10fe
Víctor Martínez <victor.martinez@tecnativa.com>
2020-10-16 11:43:13 +0200

    [MIG] contract_layout_category_hide_detail: Migration to 13.0

--- contract_layout_category_hide_detail/__manifest__.py
+++ contract_layout_category_hide_detail/__manifest__.py
@@ -4,11 +4,12 @@
 {
     "name": "Contract layout category hide detail",
     "summary": "Hide details for sections in Contract lines",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Contract Management",
     "website": "https://github.com/OCA/contract",
     "author": "Tecnativa, " "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "depends": ["contract", "sale_layout_category_hide_detail"],
+    "development_status": "Production/Stable",
     "data": ["views/abstract_contract_line.xml", "views/contract.xml"],
 }
--- contract_layout_category_hide_detail/models/contract_line.py
+++ contract_layout_category_hide_detail/models/contract_line.py
@@ -2,15 +2,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
-from odoo import api, models
+from odoo import models
 
 
 class ContractLine(models.Model):
     _inherit = "contract.line"
 
-    @api.multi
-    def _prepare_invoice_line(self, invoice_id=False, invoice_values=False):
-        vals = super()._prepare_invoice_line(invoice_id, invoice_values)
+    def _prepare_invoice_line(self, move_form):
+        vals = super()._prepare_invoice_line(move_form)
         # If the line has skip_zero_qty field (provided by contract_variable
         # quantity module) set to 'True' and 'quantity' field' is zero then
         # 'vals' will be equal to {}
