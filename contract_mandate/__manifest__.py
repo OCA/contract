311a120c879ae367c3c65802bef67ff3cb587807
Guille <guillermo@studio73.es>
2020-10-21 12:28:28 +0200

    [IMP] contract_mandate: black, isort, prettier

--- contract_mandate/__manifest__.py
+++ contract_mandate/__manifest__.py
@@ -2,21 +2,15 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Contract Mandate',
-    'summary': 'Mandate in contracts and their invoices',
-    'version': '12.0.1.0.2',
-    'author': 'Odoo Community Association (OCA), '
-              'Tecnativa',
-    'website': 'https://github.com/OCA/contract',
-    'depends': [
-        'contract_payment_mode',
-        'account_banking_mandate',
-    ],
-    'category': 'Sales Management',
-    'license': 'AGPL-3',
-    'data': [
-        'views/contract_view.xml',
-    ],
-    'installable': True,
-    'auto_install': True,
+    "name": "Contract Mandate",
+    "summary": "Mandate in contracts and their invoices",
+    "version": "12.0.1.0.2",
+    "author": "Odoo Community Association (OCA), " "Tecnativa",
+    "website": "https://github.com/OCA/contract",
+    "depends": ["contract_payment_mode", "account_banking_mandate",],
+    "category": "Sales Management",
+    "license": "AGPL-3",
+    "data": ["views/contract_view.xml",],
+    "installable": True,
+    "auto_install": True,
 }
