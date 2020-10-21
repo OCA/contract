e90c7174f033d01393df2af9a29d5ba6c931f4c1
Guille <guillermo@studio73.es>
2020-10-21 09:32:44 +0200

    [MIG] contract_payment_mode: Migration to 13.0

--- contract_payment_mode/models/contract.py
+++ contract_payment_mode/models/contract.py
@@ -15,28 +15,10 @@ class ContractContract(models.Model):
     def on_change_partner_id(self):
         self.payment_mode_id = self.partner_id.customer_payment_mode_id.id
 
-    @api.multi
     def _prepare_invoice(self, date_invoice, journal=None):
-        invoice_vals = super(ContractContract, self)._prepare_invoice(
+        invoice_vals, move_form = super()._prepare_invoice(
             date_invoice=date_invoice, journal=journal
         )
         if self.payment_mode_id:
             invoice_vals["payment_mode_id"] = self.payment_mode_id.id
-            invoice = self.env["account.invoice"].new(invoice_vals)
-            invoice._onchange_payment_mode_id()
-            invoice_vals = invoice._convert_to_write(invoice._cache)
-        return invoice_vals
-
-    @api.model
-    def _finalize_invoice_creation(self, invoices):
-        """
-        This override preserves the payment mode when calling the partner's
-        onchange.
-        """
-        payment_modes_by_invoice = {}
-        for invoice in invoices:
-            payment_modes_by_invoice[invoice] = invoice.payment_mode_id
-        res = super(ContractContract, self)._finalize_invoice_creation(invoices)
-        for invoice in invoices:
-            invoice.payment_mode_id = payment_modes_by_invoice.get(invoice)
-        return res
+        return invoice_vals, move_form
