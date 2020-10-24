0a0d23ed52547b54b1964ddcaff61f89816f15fa
sbejaoui <souheil.bejaoui@acsone.eu>
2020-10-24 20:37:58 +0200

    [13.0][MIG] - migration product_contract

--- product_contract/models/res_company.py
+++ product_contract/models/res_company.py
@@ -6,7 +6,7 @@ from odoo import fields, models
 
 class ResCompany(models.Model):
 
-    _inherit = 'res.company'
+    _inherit = "res.company"
 
     create_contract_at_sale_order_confirmation = fields.Boolean(
         string="Automatically Create Contracts At Sale Order Confirmation",
