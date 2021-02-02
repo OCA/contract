52d24a77aebd7374d5fa995d616f30c6e0040ca1
Víctor Martínez <victor.martinez@tecnativa.com>
2021-02-02 10:34:23 +0100

    [FIX] contract: Error in test_contract_invoice_followers

--- contract/__manifest__.py
+++ contract/__manifest__.py
@@ -11,7 +11,7 @@
 
 {
     "name": "Recurring - Contracts Management",
-    "version": "13.0.2.3.2",
+    "version": "13.0.2.3.3",
     "category": "Contract Management",
     "license": "AGPL-3",
     "author": "Tecnativa, ACSONE SA/NV, Odoo Community Association (OCA)",
