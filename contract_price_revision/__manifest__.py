a64c7f03f20202311766cf71928f71241938ea27
Denis Roussel <denis.roussel@acsone.eu>
2021-02-14 19:11:42 +0100

    [IMP] contract_price_revision: black, isort, prettier

--- contract_price_revision/__manifest__.py
+++ contract_price_revision/__manifest__.py
@@ -3,22 +3,16 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Contract Price Revision',
-    'summary': 'Easy revision of contract prices',
-    'version': '12.0.1.0.1',
-    'category': 'Contract',
-    'author': 'Tecnativa, '
-              'Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'website': 'https://github.com/OCA/contract',
-    'depends': [
-        'contract',
-    ],
-    'data': [
-        'views/contract_line.xml',
-        'wizards/contract_price_revision_views.xml',
-    ],
-    'installable': True,
-    'development_status': 'Production/Stable',
-    'maintainers': ['carlosdauden'],
+    "name": "Contract Price Revision",
+    "summary": "Easy revision of contract prices",
+    "version": "12.0.1.0.1",
+    "category": "Contract",
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/contract",
+    "depends": ["contract",],
+    "data": ["views/contract_line.xml", "wizards/contract_price_revision_views.xml",],
+    "installable": True,
+    "development_status": "Production/Stable",
+    "maintainers": ["carlosdauden"],
 }
