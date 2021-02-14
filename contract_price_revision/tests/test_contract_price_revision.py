aac2496a1b5e9eb13cedca54ba9712d5f8b4d978
Denis Roussel <denis.roussel@acsone.eu>
2021-02-14 19:11:42 +0100

    [IMP] contract_price_revision: black, isort, prettier

--- contract_price_revision/tests/test_contract_price_revision.py
+++ contract_price_revision/tests/test_contract_price_revision.py
@@ -6,32 +6,36 @@ from odoo.addons.contract.tests.test_contract import TestContractBase
 
 
 class TestContractPriceRevision(TestContractBase):
-
     def execute_wizard(self):
-        wizard = self.env['contract.price.revision.wizard'].create({
-            'date_start': '2018-02-15',
-            'variation_percent': 100.0,
-        })
-        wizard.with_context(
-            {'active_ids': [self.contract.id]}).action_apply()
+        wizard = self.env["contract.price.revision.wizard"].create(
+            {"date_start": "2018-02-15", "variation_percent": 100.0,}
+        )
+        wizard.with_context({"active_ids": [self.contract.id]}).action_apply()
 
     def test_contract_price_revision_wizard(self):
         # This is for checking if this line is not versioned
-        self.acct_line.copy({'automatic_price': True})
+        self.acct_line.copy({"automatic_price": True})
         self.assertEqual(len(self.contract.contract_line_ids.ids), 2)
         self.execute_wizard()
         self.assertEqual(len(self.contract.contract_line_ids.ids), 3)
         lines = self.contract.contract_line_ids.filtered(
-            lambda x: x.price_unit == 200.0)
+            lambda x: x.price_unit == 200.0
+        )
         self.assertEqual(len(lines), 1)
 
     def test_contract_price_revision_invoicing(self):
-        self.acct_line.copy({'automatic_price': True})
+        self.acct_line.copy({"automatic_price": True})
         self.execute_wizard()
         invoice = self.contract.recurring_create_invoice()
-        invoices = self.env['account.invoice'].search([
-            ('invoice_line_ids.contract_line_id', 'in',
-             self.contract.contract_line_ids.ids)])
+        invoices = self.env["account.invoice"].search(
+            [
+                (
+                    "invoice_line_ids.contract_line_id",
+                    "in",
+                    self.contract.contract_line_ids.ids,
+                )
+            ]
+        )
         self.assertEqual(len(invoices), 1)
         lines = invoice.invoice_line_ids
         self.assertEqual(len(lines), 2)
--- contract_price_revision/wizards/contract_price_revision.py
+++ contract_price_revision/wizards/contract_price_revision.py
@@ -5,60 +5,57 @@
 from dateutil.relativedelta import relativedelta
 
 from odoo import fields, models
+
 import odoo.addons.decimal_precision as dp
 
 
 class ContractPriceRevisionWizard(models.TransientModel):
     """ Update contract price based on percentage variation """
-    _name = 'contract.price.revision.wizard'
+
+    _name = "contract.price.revision.wizard"
     _description = "Wizard to update price based on percentage variation"
 
-    date_start = fields.Date(
-        required=True,
-    )
+    date_start = fields.Date(required=True,)
     date_end = fields.Date()
     variation_percent = fields.Float(
-        digits=dp.get_precision('Product Price'),
-        required=True,
-        string='Variation %',
+        digits=dp.get_precision("Product Price"), required=True, string="Variation %",
     )
 
     def action_apply(self):
-        ContractLine = self.env['contract.line']
-        active_ids = self.env.context.get('active_ids')
-        contracts = self.env['contract.contract'].browse(active_ids)
+        ContractLine = self.env["contract.line"]
+        active_ids = self.env.context.get("active_ids")
+        contracts = self.env["contract.contract"].browse(active_ids)
         for line in self._get_contract_lines_to_revise(contracts):
-            line.update({
-                'date_end': self.date_start - relativedelta(days=1),
-            })
-            new_vals = line.copy_data({
-                'date_start': self.date_start,
-                'date_end': self.date_end,
-                'predecessor_contract_line_id': line.id,
-                'price_unit': line.price_unit * (
-                    1.0 + self.variation_percent / 100.0),
-            })[0]
+            line.update(
+                {"date_end": self.date_start - relativedelta(days=1),}
+            )
+            new_vals = line.copy_data(
+                {
+                    "date_start": self.date_start,
+                    "date_end": self.date_end,
+                    "predecessor_contract_line_id": line.id,
+                    "price_unit": line.price_unit
+                    * (1.0 + self.variation_percent / 100.0),
+                }
+            )[0]
             tmp_line = ContractLine.new(new_vals)
             tmp_line._onchange_date_start()
-            new_line = ContractLine.create(
-                tmp_line._convert_to_write(tmp_line._cache))
-            line.update({
-                'successor_contract_line_id': new_line.id,
-            })
-        action = self.env['ir.actions.act_window'].for_xml_id(
-            'contract', 'action_customer_contract')
-        action['domain'] = [('id', 'in', active_ids)]
+            new_line = ContractLine.create(tmp_line._convert_to_write(tmp_line._cache))
+            line.update(
+                {"successor_contract_line_id": new_line.id,}
+            )
+        action = self.env["ir.actions.act_window"].for_xml_id(
+            "contract", "action_customer_contract"
+        )
+        action["domain"] = [("id", "in", active_ids)]
         return action
 
     def _get_contract_lines_to_revise(self, contracts):
         self.ensure_one()
-        to_revise = (
-            contracts.mapped("contract_line_ids")
-            .filtered(
-                lambda x: not x.automatic_price
-                and not x.successor_contract_line_id
-                and x.recurring_next_date
-                and (not x.date_end or x.date_end >= self.date_start)
-            )
+        to_revise = contracts.mapped("contract_line_ids").filtered(
+            lambda x: not x.automatic_price
+            and not x.successor_contract_line_id
+            and x.recurring_next_date
+            and (not x.date_end or x.date_end >= self.date_start)
         )
         return to_revise
