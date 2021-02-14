dadee755d22756f522967b1c08eaffb3e1af5e3a
sergio-teruel <sergio.teruel@tecnativa.com>
2021-02-14 09:20:21 +0100

    [IMP] agreement: black, isort, prettier

--- agreement/__manifest__.py
+++ agreement/__manifest__.py
@@ -2,27 +2,24 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Agreement',
-    'summary': "Adds an agreement object",
-    'version': '12.0.1.2.0',
-    'category': 'Contract',
-    'author': 'Akretion, '
-              'Yves Goldberg (Ygol Internetwork), '
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/oca/contract',
-    'license': 'AGPL-3',
-    'depends': ['mail'],
-    'data': [
-        'security/ir.model.access.csv',
-        'security/agreement_security.xml',
-        'views/agreement.xml',
-        'views/agreement_type.xml',
-        ],
-    'demo': ['demo/demo.xml'],
-    'development_status': 'Beta',
-    'maintainers': [
-        'ygol',
-        'alexis-via',
+    "name": "Agreement",
+    "summary": "Adds an agreement object",
+    "version": "12.0.1.2.0",
+    "category": "Contract",
+    "author": "Akretion, "
+    "Yves Goldberg (Ygol Internetwork), "
+    "Odoo Community Association (OCA)",
+    "website": "https://github.com/oca/contract",
+    "license": "AGPL-3",
+    "depends": ["mail"],
+    "data": [
+        "security/ir.model.access.csv",
+        "security/agreement_security.xml",
+        "views/agreement.xml",
+        "views/agreement_type.xml",
     ],
-    'installable': True,
+    "demo": ["demo/demo.xml"],
+    "development_status": "Beta",
+    "maintainers": ["ygol", "alexis-via",],
+    "installable": True,
 }
