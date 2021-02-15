127f4a696ffa000c8e43e67570802d98fb087fd1
Denis Roussel <denis.roussel@acsone.eu>
2021-02-15 10:24:40 +0100

    [13.0][IMP] contract_price_revision: Add fixed price revision

--- contract_price_revision/wizards/contract_price_revision.py
+++ contract_price_revision/wizards/contract_price_revision.py
@@ -5,9 +5,7 @@
 
 from dateutil.relativedelta import relativedelta
 
-from odoo import fields, models
-
-import odoo.addons.decimal_precision as dp
+from odoo import api, fields, models
 
 
 class ContractPriceRevisionWizard(models.TransientModel):
@@ -18,9 +16,33 @@ class ContractPriceRevisionWizard(models.TransientModel):
 
     date_start = fields.Date(required=True,)
     date_end = fields.Date()
-    variation_percent = fields.Float(
-        digits=dp.get_precision("Product Price"), required=True, string="Variation %",
+    variation_type = fields.Selection(
+        selection=lambda self: self._get_variation_type(),
+        required=True,
+        default=lambda self: self._get_default_variation_type(),
     )
+    variation_percent = fields.Float(digits="Product Price", string="Variation %",)
+    fixed_price = fields.Float(digits="Product Price")
+
+    @api.model
+    def _get_variation_type(self):
+        return [
+            ("percentage", "Percentage"),
+            ("fixed", "Fixed Price"),
+        ]
+
+    @api.model
+    def _get_default_variation_type(self):
+        return "percentage"
+
+    def _get_new_price(self, line):
+        """Get the price depending the change type chosen
+        """
+        if self.variation_type == "percentage":
+            return line.price_unit * (1.0 + self.variation_percent / 100.0)
+        elif self.variation_type == "fixed":
+            return self.fixed_price
+        return line.price_unit
 
     def _get_new_line_value(self, line):
         self.ensure_one()
@@ -29,17 +51,21 @@ class ContractPriceRevisionWizard(models.TransientModel):
             "last_date_invoiced": False,
             "date_end": self.date_end,
             "predecessor_contract_line_id": line.id,
-            "price_unit": line.price_unit * (1.0 + self.variation_percent / 100.0),
+            "price_unit": self._get_new_price(line),
         }
+
+    def _get_old_line_date_end(self, line):
+        return self.date_start - relativedelta(days=1)
 
     def action_apply(self):
         ContractLine = self.env["contract.line"]
         active_ids = self.env.context.get("active_ids")
         contracts = self.env["contract.contract"].browse(active_ids)
         for line in self._get_contract_lines_to_revise(contracts):
-            line.update({"date_end": self.date_start - relativedelta(days=1)})
+            date_end = self._get_old_line_date_end(line)
+            line.stop(date_end)
             # As copy or copy_data are trigerring constraints, don't use them
-            new_line = line.new(line._cache)
+            new_line = self.env["contract.line"].new(line._cache)
             new_line.update(self._get_new_line_value(line))
             new_line._onchange_date_start()
             new_line = ContractLine.create(new_line._convert_to_write(new_line._cache))
