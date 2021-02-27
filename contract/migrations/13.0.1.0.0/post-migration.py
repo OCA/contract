6d43d343ea5cb664fa44fe4303761b1254df23b2
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-02-27 13:41:48 +0100

    [OU-FIX] contract: Don't alter v12 recurrence mechanism
    We can't guarantee the homogeneity of the existing contracts, so we
    keep the line recurrence behavior.

--- contract/migrations/13.0.1.0.0/post-migration.py
+++ contract/migrations/13.0.1.0.0/post-migration.py
@@ -1,4 +1,4 @@
-# Copyright 2020 Tecnativa - Pedro M. Baeza
+# Copyright 2020-2021 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openupgradelib import openupgrade  # pylint: disable=W7936
@@ -9,3 +9,6 @@ def migrate(env, version):
     openupgrade.load_data(
         env.cr, "contract", "migrations/13.0.1.0.0/noupdate_changes.xml"
     )
+    # Don't alter line recurrence v12 behavior
+    contracts = env["contract.contract"].search([])
+    contracts.write({"line_recurrence": True})
