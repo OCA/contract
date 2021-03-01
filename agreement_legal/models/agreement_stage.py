0f61784096da9627acf46173fe659f61853126f4
newtratip <tharathip.chaweewongphan@gmail.com>
2021-03-01 16:40:18 +0700

    [12.0][FIX] agreement_legal: Add active field on Agreement Stage

--- agreement_legal/models/agreement_stage.py
+++ agreement_legal/models/agreement_stage.py
@@ -29,3 +29,6 @@ class AgreementStage(models.Model):
     stage_type = fields.Selection(
         [('agreement', 'Agreement')],
         string='Type', required=True)
+    active = fields.Boolean(
+        string="Active",
+        default=True)
