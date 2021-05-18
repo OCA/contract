2cb67a1fdb6776f8d17584d7427bf27a2027e3ba
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-05-18 20:55:47 +0200

    [FIX] contract: Don't change period start date having line recurrence
    Fine-tuning of cd086ddbb4a9b85ccfcac288e53fb93e716d9446. We shouldn't do
    this adjustment if the line recurrence is set, as if so, there's no
    possibility of adding post-paid lines with proper dates on a running
    contract.

--- contract/__manifest__.py
+++ contract/__manifest__.py
@@ -11,7 +11,7 @@
 
 {
     "name": "Recurring - Contracts Management",
-    "version": "13.0.2.3.8",
+    "version": "13.0.2.3.9",
     "category": "Contract Management",
     "license": "AGPL-3",
     "author": "Tecnativa, ACSONE SA/NV, Odoo Community Association (OCA)",
