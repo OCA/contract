f69e0aa1a9451582d779220b4e6215240fbe65c8
hkapatel <helly.kapatel@initos.com>
2021-06-17 11:21:42 +0530

    [IMP] agreement_project: black, isort, prettier

--- agreement_project/models/agreement.py
+++ agreement_project/models/agreement.py
@@ -7,12 +7,12 @@ from odoo import api, fields, models
 class Agreement(models.Model):
     _inherit = "agreement"
 
-    task_count = fields.Integer('# Tasks',
-                                compute='_compute_task_count')
+    task_count = fields.Integer("# Tasks", compute="_compute_task_count")
 
     @api.multi
     def _compute_task_count(self):
         for ag in self:
-            count = self.env['project.task'].search_count(
-                [('agreement_id', '=', ag.id)])
+            count = self.env["project.task"].search_count(
+                [("agreement_id", "=", ag.id)]
+            )
             ag.task_count = count
