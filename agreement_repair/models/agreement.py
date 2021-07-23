30b336e1d893c3f186ca4b8caf6b43be33e5b5aa
Vimal Patel <vimal.patel.serpentcs@gmail.com>
2021-07-23 13:08:35 +0530

    [14.0][MIG]agreement_repair: Migrated to v14

--- agreement_repair/models/agreement.py
+++ agreement_repair/models/agreement.py
@@ -1,7 +1,7 @@
-# Copyright (C) 2018 - TODAY, Open Source Integrators
+# Copyright (C) 2021 - TODAY, Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class Agreement(models.Model):
@@ -9,7 +9,6 @@ class Agreement(models.Model):
 
     repair_count = fields.Integer("# Repair Orders", compute="_compute_repair_count")
 
-    @api.multi
     def _compute_repair_count(self):
         for ag_rec in self:
             ag_rec.repair_count = self.env["repair.order"].search_count(
