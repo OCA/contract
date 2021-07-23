3dfd2801360f2f5b04948e1158788e3a73bb28a0
Vimal Patel <vimal.patel.serpentcs@gmail.com>
2021-07-23 13:08:35 +0530

    [14.0][MIG]agreement_repair: Migrated to v14

--- agreement_repair/models/repair.py
+++ agreement_repair/models/repair.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2018 - TODAY, Open Source Integrators
+# Copyright (C) 2021 - TODAY, Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
