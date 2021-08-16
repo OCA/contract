ffaa36d0b1aad1d53de0bff57511f08232697be7
HemanginiPatel <h.patel.serpentcs@gmail.com>
2021-08-16 16:08:14 +0530

    [IMP]Improved the UT.

--- agreement_legal_sale/tests/test_sale_order.py
+++ agreement_legal_sale/tests/test_sale_order.py
@@ -18,9 +18,26 @@ class TestSaleOrder(TransactionCase):
                 "domain": "sale",
             }
         )
+        self.test_account_analytic_account = self.env[
+            "account.analytic.account"
+        ].create({"name": "Test Analytic Account"})
         self.test_agreement_template = self.env["agreement"].create(
             {
                 "name": "TestAgreementTemplate",
+                "description": "Test Template",
+                "special_terms": "Test Template",
+                "is_template": True,
+                "partner_id": self.test_customer.id,
+                "start_date": fields.Date.today(),
+                "end_date": fields.Date.today() + timedelta(days=365),
+                "analytic_account_id": self.test_account_analytic_account.id,
+            }
+        )
+        self.test_agreement_template_no_analytic_account_id = self.env[
+            "agreement"
+        ].create(
+            {
+                "name": "TestAgreementTemplate A",
                 "description": "Test Template",
                 "special_terms": "Test Template",
                 "is_template": True,
@@ -40,6 +57,33 @@ class TestSaleOrder(TransactionCase):
                 ],
             }
         )
+        self.test_product_templ_is_serviceprofile = self.env["product.template"].create(
+            {"name": "TestProductA", "is_serviceprofile": True}
+        )
+        self.test_product_is_serviceprofile = self.env["product.product"].create(
+            {
+                "name": "TestProductA",
+                "is_serviceprofile": True,
+                "product_tmpl_id": self.test_product_templ_is_serviceprofile.id,
+            }
+        )
+        self.test_sale_order_is_serviceprofile = self.env["sale.order"].create(
+            {
+                "partner_id": self.test_customer.id,
+                "agreement_template_id": self.test_agreement_template.id,
+                "date_order": fields.Date.today(),
+                "order_line": [
+                    (
+                        0,
+                        0,
+                        {
+                            "product_id": self.test_product_is_serviceprofile.id,
+                            "product_uom_qty": 1.0,
+                        },
+                    )
+                ],
+            }
+        )
 
     # TEST 01: Test _action_confirm method
     def test_action_confirm(self):
@@ -49,4 +93,15 @@ class TestSaleOrder(TransactionCase):
         self.assertEqual(
             agreement.sale_id.id,
             sale_order.id,
+        )
+
+        # Test 02: is_serviceprofile Product
+        is_serviceprofile_so = self.test_sale_order_is_serviceprofile
+        is_serviceprofile_so._action_confirm()
+        agreement = self.env["agreement"].search(
+            [("sale_id", "=", is_serviceprofile_so.id)]
+        )
+        self.assertEqual(
+            agreement.sale_id.id,
+            is_serviceprofile_so.id,
         )
