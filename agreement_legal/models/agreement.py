27a348074852596cbc81ed5f4d8d61698e13f329
Olga Marco <olga.marco@creublanca.es>
2021-10-15 12:13:21 +0200

    [IMP] agreement_legal: Define warning days cron and filter

--- agreement_legal/models/agreement.py
+++ agreement_legal/models/agreement.py
@@ -3,6 +3,7 @@
 
 import ast
 import json as simplejson
+from datetime import timedelta
 
 from lxml import etree
 
@@ -288,6 +289,41 @@ class Agreement(models.Model):
         "agreement", string="Template", domain=[("is_template", "=", True)],
     )
     readonly = fields.Boolean(related="stage_id.readonly",)
+    to_review_date = fields.Date(
+        compute="_compute_to_review_date",
+        store=True,
+        readonly=False,
+        help="Date used to warn us some days before agreement expires",
+    )
+
+    @api.depends("agreement_type_id", "end_date")
+    def _compute_to_review_date(self):
+        for record in self:
+            if record.end_date:
+                record.to_review_date = record.end_date + timedelta(
+                    days=-record.agreement_type_id.review_days
+                )
+
+    @api.model
+    def _alert_to_review_date(self):
+        activities = self.search(
+            [
+                ("to_review_date", "=", fields.datetime.today()),
+                ("agreement_type_id.review_user_id", "!=", False),
+            ]
+        )
+        for activity in activities:
+            if (
+                self.env["mail.activity"].search_count(
+                    [("res_id", "=", activity.id), ("res_model", "=", "agreement")]
+                )
+                == 0
+            ):
+                activity.activity_schedule(
+                    "agreement_legal.mail_activity_review_agreement",
+                    user_id=activity.type_id.review_user_id.id,
+                    note=_("Your activity is going to end soon"),
+                )
 
     # compute the dynamic content for jinja expression
     def _compute_dynamic_description(self):
