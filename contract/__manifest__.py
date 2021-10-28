04a6248288d76a7fe30197fb2e7ef5e132f85cb6
Jean-Charles Drubay <jc@komit-consulting.com>
2021-10-28 18:24:51 +0700

    [MIG] contract: Migration to 15.0
    Most changes are related to the switch from jinja to qweb in mail templates.
    
    Also included:
    - convert deprecated onchange that returns a domain and other deprecation warnings
      (see below)
    - Add migration scripts from version 14.0 (force the update of the mail templates)
    - Fix warnings from pre-commit checks
    
    Fixes depreciation warnings:
    
    - onchange method ContractAbstractContractLine._onchange_product_id returned
      a domain, this is deprecated
    - SavepointCase is deprecated:
      https://github.com/odoo/odoo/blob/15.0/odoo/tests/common.py#L742
    - assertDictContainsSubset: According to:
      https://stackoverflow.com/questions/20050913/python-unittests-assertdictcontainssubset-recommended-alternative

--- contract/__manifest__.py
+++ contract/__manifest__.py
@@ -11,7 +11,7 @@
 
 {
     "name": "Recurring - Contracts Management",
-    "version": "14.0.1.2.3",
+    "version": "15.0.1.0.0",
     "category": "Contract Management",
     "license": "AGPL-3",
     "author": "Tecnativa, ACSONE SA/NV, Odoo Community Association (OCA)",
@@ -35,7 +35,6 @@
         "wizards/contract_manually_create_invoice.xml",
         "wizards/contract_contract_terminate.xml",
         "views/contract_tag.xml",
-        "views/assets.xml",
         "views/abstract_contract_line.xml",
         "views/contract.xml",
         "views/contract_line.xml",
@@ -46,5 +45,12 @@
         "views/contract_terminate_reason.xml",
         "views/contract_portal_templates.xml",
     ],
+    "assets": {
+        "web.assets_backend": [
+            "contract/static/src/js/section_and_note_fields_backend.js",
+        ],
+        "web.assets_frontend": ["contract/static/src/scss/frontend.scss"],
+        "web.assets_tests": ["contract/static/src/js/contract_portal_tour.js"],
+    },
     "installable": True,
 }
