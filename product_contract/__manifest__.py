27c9192fde401f35a8fee192f9c92e05e7f7e8ec
iammiracle <miracleaayodele@gmail.com>
2021-11-09 09:06:37 +0100

    manifest fix

--- product_contract/__manifest__.py
+++ product_contract/__manifest__.py
@@ -8,7 +8,7 @@
     "category": "Contract Management",
     "license": "AGPL-3",
     "author": "LasLabs, " "ACSONE SA/NV, " "Odoo Community Association (OCA), " "Vertel AB",
-    "website": "https://github.com/oca/contract",
+    # "website": "https://github.com/oca/contract",
     "depends": ["product", "contract_sale"],
     "data": [
         "views/res_config_settings.xml",
