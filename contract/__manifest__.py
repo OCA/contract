850238eb843f6e1155371781c9d8cb34cc32a532
Joan Mateu Jordi <joan.mateu@forgeflow.com>
2022-01-25 11:04:13 +0100

    [15.0][IMP] modify production beta to stable

--- contract/__manifest__.py
+++ contract/__manifest__.py
@@ -11,12 +11,13 @@
 
 {
     "name": "Recurring - Contracts Management",
-    "version": "15.0.1.0.1",
+    "version": "15.0.1.0.2",
     "category": "Contract Management",
     "license": "AGPL-3",
     "author": "Tecnativa, ACSONE SA/NV, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/contract",
     "depends": ["base", "account", "product", "portal"],
+    "development_status": "Production/Stable",
     "external_dependencies": {"python": ["dateutil"]},
     "data": [
         "security/groups.xml",
