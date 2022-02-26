8e76b9d59c798cf54b4af87fd95e2e1452f3c0a7
Mosab Wadea <mwaamh@gmail.com>
2022-02-26 22:07:26 +0300

    [MIG] agreement_account: Migration to 15.0

--- agreement_account/__manifest__.py
+++ agreement_account/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Agreement Account",
     "summary": "Agreement on invoices",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Contract",
     "author": "Akretion, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/contract",
