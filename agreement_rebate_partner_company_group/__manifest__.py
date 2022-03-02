dc36c9101f13b5addea3d76704ab53346d519b92
Carlos Dauden <carlos.dauden@tecnativa.com>
2022-03-02 17:14:10 +0100

    [ADD] agreement_rebate_partner_company_group: New module that apply agreement to all company group members TT34815

---
+++ agreement_rebate_partner_company_group/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 Tecnativa - Carlos Dauden
+# Copyright 2022 Tecnativa - Sergio Teruel
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Agreement Rebate Partner Company Group",
+    "summary": "Rebate agreements applied to all company group members",
+    "version": "13.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Contract",
+    "website": "https://github.com/OCA/contract",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["agreement_rebate", "partner_company_group"],
+    "data": [],
+}
---
+++ agreement_rebate_partner_company_group/readme/USAGE.rst
@@ -0,0 +1,6 @@
+To use this module you need to:
+
+#. Create an agreement related with a company group partner.
+#. Create several invoices related with company group members.
+#. Create rebate settlements from *Agreements > Settlements > Create Settlements*
+   wizard
