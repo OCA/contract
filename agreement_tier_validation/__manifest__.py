d78b6787bd8f78e3cff69e57e108c62f2ea6fb7a
Kitti U <kittiu@ecosoft.co.th>
2022-03-29 10:54:11 +0700

    [14.0][ADD] agreement_tier_validation

---
+++ agreement_tier_validation/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2022 Ecosoft Co., Ltd.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Agreement Tier Validation",
+    "summary": "Extends the functionality of Agreement to "
+    "support a tier validation process.",
+    "version": "14.0.1.0.0",
+    "category": "Contract Management",
+    "website": "https://github.com/OCA/contract",
+    "author": "Ecosoft, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["agreement_legal", "base_tier_validation"],
+    "data": ["views/agreement_view.xml"],
+}
