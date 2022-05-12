50ef8f33139d9f6b550bca9e6e8150f62ec2c75b
Chafique <chafique.delli@akretion.com>
2022-05-12 18:24:34 +0200

    [MIG] contract_invoice_start_end_dates: Migration to 14.0

--- contract_invoice_start_end_dates/__manifest__.py
+++ contract_invoice_start_end_dates/__manifest__.py
@@ -2,18 +2,17 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    'name': 'Contract Invoice Start End Dates',
-    'version': '12.0.1.0.0',
-    'category': 'Contract Management',
-    'author': 'Akretion, '
-              'Odoo Community Association (OCA)',
-    'maintainers': ['florian-dacosta'],
-    'website': 'https://github.com/OCA/contract',
-    'depends': [
-        'account_invoice_start_end_dates',
-        'contract',
+    "name": "Contract Invoice Start End Dates",
+    "version": "14.0.1.0.0",
+    "category": "Contract Management",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "maintainers": ["florian-dacosta"],
+    "website": "https://github.com/OCA/contract",
+    "depends": [
+        "account_invoice_start_end_dates",
+        "contract",
     ],
-    'data': [],
-    'license': 'AGPL-3',
-    'installable': True,
+    "data": [],
+    "license": "AGPL-3",
+    "installable": True,
 }
