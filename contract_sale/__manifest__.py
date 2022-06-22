564463fb332fac539fe963705a34cca0426c66d9
Denis Roussel <denis.roussel@acsone.eu>
2022-06-22 11:04:58 +0200

    [FIX] contract_sale: Good test import

--- contract_sale/__manifest__.py
+++ contract_sale/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Contract from Sale",
-    "version": "15.0.1.0.0",
+    "version": "15.0.1.0.1",
     "category": "Sales",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/contract",
