9043b48bf4469dbee57bddbcd83e68b93507bdb0
Denis Roussel <denis.roussel@acsone.eu>
2022-08-07 23:41:51 +0200

    [IMP] contract_price_revision: Improve readability by adding a computed field 'price_can_be_revised'

--- contract_price_revision/models/contract_line.py
+++ contract_price_revision/models/contract_line.py
@@ -20,6 +20,24 @@ class ContractLine(models.Model):
         string="Variation %",
     )
 
+    price_can_be_revised = fields.Boolean(
+        compute="_compute_price_can_be_revised",
+        help="Technical field in order to know if the line price can be revised.",
+    )
+
+    @api.depends_context("date_start")
+    def _compute_price_can_be_revised(self):
+        date_start = self.env.context.get("date_start", fields.Datetime.now())
+        lines_can_be_revised = self.filtered(
+            lambda line: not line.automatic_price
+            and not line.successor_contract_line_id
+            and line.recurring_next_date
+            and not line.display_type
+            and (not line.date_end or line.date_end >= date_start)
+        )
+        lines_can_be_revised.price_can_be_revised = True
+        (self - lines_can_be_revised).price_can_be_revised = False
+
     @api.depends("price_unit", "predecessor_contract_line_id.price_unit")
     def _compute_variation_percent(self):
         for line in self:
