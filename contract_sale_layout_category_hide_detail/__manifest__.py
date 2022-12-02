0899e5c7c34d74c32a291ade769fd65b779dae09
gael <gael.torrecillas@mind-and-go.com>
2022-12-02 14:51:12 +0100

    [14.0][new] contract sale layout_category_hide_detail

---
+++ contract_sale_layout_category_hide_detail/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2018-2019 Tecnativa - Ernesto Tejeda
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Contract sale layout category hide detail",
+    "summary": "Correlation between contract and quote lines for hiden fields",
+    "version": "14.0.1.0.0",
+    "category": "Contract Management",
+    "website": "https://github.com/OCA/contract",
+    "author": "Mind And Go " "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": ["contract_layout_category_hide_detail","contract_sale_generation"],
+    "data": [],
+}
---
+++ contract_sale_layout_category_hide_detail/models/contract_line.py
@@ -0,0 +1,15 @@
+from odoo import models
+
+
+class ContractLine(models.Model):
+    _inherit = "contract.line"
+
+    def _prepare_sale_line_vals(self, dates, order_id=False):
+        sale_line_vals = super(ContractLine,self)._prepare_sale_line_vals(dates,order_id)
+        sale_line_vals.update( {
+                "show_details":self.show_details, 
+                "show_subtotal":self.show_subtotal,
+                "show_section_subtotal":self.show_section_subtotal, 
+                "show_line_amount":self.show_line_amount,
+        })
+        return sale_line_vals
