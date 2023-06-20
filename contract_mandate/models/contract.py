6fa5f4363bcefaa06bc952c1c33ce5dafdc3d596
aiendry-aktivsoftware <aiendry@aktivsoftware.com>
2023-06-20 11:23:08 +0530

    [MIG] contract_mandate: Migration to 16.0

--- contract_mandate/models/contract.py
+++ contract_mandate/models/contract.py
@@ -16,7 +16,8 @@ class ContractContract(models.Model):
         index=True,
     )
     mandate_required = fields.Boolean(
-        related="payment_mode_id.payment_method_id.mandate_required", readonly=True
+        related="payment_mode_id.payment_method_id.mandate_required",
+        readonly=True,
     )
     commercial_partner_id = fields.Many2one(
         related="partner_id.commercial_partner_id",
@@ -31,19 +32,21 @@ class ContractContract(models.Model):
             self.mandate_id = False
 
     def _prepare_invoice(self, date_invoice, journal=None):
-        invoice_vals, move_form = super()._prepare_invoice(
-            date_invoice, journal=journal
-        )
+        invoice_vals = super()._prepare_invoice(date_invoice, journal=journal)
         if self.mandate_id:
             invoice_vals["mandate_id"] = self.mandate_id.id
         elif self.payment_mode_id.payment_method_id.mandate_required:
             mandate = self.env["account.banking.mandate"].search(
                 [
-                    ("partner_id", "=", self.partner_id.commercial_partner_id.id),
+                    (
+                        "partner_id",
+                        "=",
+                        self.partner_id.commercial_partner_id.id,
+                    ),
                     ("state", "=", "valid"),
                     ("company_id", "=", self.company_id.id),
                 ],
                 limit=1,
             )
             invoice_vals["mandate_id"] = mandate.id
-        return invoice_vals, move_form
+        return invoice_vals
