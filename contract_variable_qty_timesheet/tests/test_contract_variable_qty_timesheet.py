d16adf0b98487beaef85594ab95e52e56bdfa99a
Carolina Fernandez <carolina.fernandez@tecnativa.com>
2023-09-28 08:18:31 -0300

    [MIG] contract_variable_qty_timesheet: Migration to 15.0
    [UPD] Update contract_variable_qty_timesheet.pot
    [BOT] post-merge updates
    Update translation files
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: contract-15.0/contract-15.0-contract_variable_qty_timesheet
    Translate-URL: https://translation.odoo-community.org/projects/contract-15-0/contract-15-0-contract_variable_qty_timesheet/

--- contract_variable_qty_timesheet/tests/test_contract_variable_qty_timesheet.py
+++ contract_variable_qty_timesheet/tests/test_contract_variable_qty_timesheet.py
@@ -5,7 +5,7 @@ from odoo import fields
 from odoo.tests import common
 
 
-class TestContractVariableQtyTimesheet(common.SavepointCase):
+class TestContractVariableQtyTimesheet(common.TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
