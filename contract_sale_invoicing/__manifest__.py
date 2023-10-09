3fad6bfa491557a59256144f00e83c6393925f88
Carolina Fernandez <carolina.fernandez@tecnativa.com>
2023-10-09 12:38:23 -0300

    [IMP] contract_sale_invoicing: pre-commit stuff

--- contract_sale_invoicing/__manifest__.py
+++ contract_sale_invoicing/__manifest__.py
@@ -1,20 +1,19 @@
 # Copyright 2018 Tecnativa - Carlos Dauden
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Contract Invoicing of Pending Sales Orders',
-    'summary': 'Include sales to invoice in contract invoice creation',
-    'version': '12.0.1.0.4',
-    'category': 'Contract Management',
-    'website': 'https://github.com/OCA/contract',
-    'author': 'Tecnativa, '
-              'Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'installable': True,
-    'depends': [
-        'contract',
-        'sale_management',
+    "name": "Contract Invoicing of Pending Sales Orders",
+    "summary": "Include sales to invoice in contract invoice creation",
+    "version": "12.0.1.0.4",
+    "category": "Contract Management",
+    "website": "https://github.com/OCA/contract",
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "contract",
+        "sale_management",
     ],
-    'data': [
-        'views/contract_view.xml',
+    "data": [
+        "views/contract_view.xml",
     ],
 }
