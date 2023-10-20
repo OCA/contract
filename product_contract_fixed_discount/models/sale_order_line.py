3c37b2c5577fab07b448a10ade061bbe048a168a
Pierre Verkest <pierreverkest84@gmail.com>
2023-10-20 15:25:25 +0200

    Apply suggestions from code review

--- product_contract_fixed_discount/models/sale_order_line.py
+++ product_contract_fixed_discount/models/sale_order_line.py
@@ -1,3 +1,7 @@
+# Copyright 2023 Foodles (https://www.foodles.com/)
+# @author Pierre Verkest <pierreverkest84@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from odoo import models
 
 
--- product_contract_fixed_discount/tests/test_sale_order.py
+++ product_contract_fixed_discount/tests/test_sale_order.py
@@ -1,5 +1,3 @@
-# Copyright 2017 LasLabs Inc.
-# Copyright 2018 ACSONE SA/NV
 # Copyright 2023 Foodles
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
