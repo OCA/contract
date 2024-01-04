b647869b4f042482f24c6cb4be995add13c13e0f
fkantelberg <florian.kantelberg@initos.com>
2024-01-04 09:44:21 +0100

    [MIG] contract: Remove analytic_account_id and add migration

--- contract/__manifest__.py
+++ contract/__manifest__.py
@@ -11,7 +11,7 @@
 
 {
     "name": "Recurring - Contracts Management",
-    "version": "16.0.1.1.3",
+    "version": "16.0.2.0.0",
     "category": "Contract Management",
     "license": "AGPL-3",
     "author": "Tecnativa, ACSONE SA/NV, Odoo Community Association (OCA)",
