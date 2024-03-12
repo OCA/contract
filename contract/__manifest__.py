dea4b29428dbbc61abc6385bf79422472eb6cb7c
Víctor Martínez <victor.martinez@tecnativa.com>
2024-03-12 13:13:34 +0100

    [IMP] contract: Remove buttons from contract lines if recurrence is not per line
    If we manage the contract at header level, we must set the end date and last invoice
    date there, otherwise data will be inconsistent.
    
    TT48273

--- contract/__manifest__.py
+++ contract/__manifest__.py
@@ -11,7 +11,7 @@
 
 {
     "name": "Recurring - Contracts Management",
-    "version": "16.0.2.4.0",
+    "version": "16.0.2.6.0",
     "category": "Contract Management",
     "license": "AGPL-3",
     "author": "Tecnativa, ACSONE SA/NV, Odoo Community Association (OCA)",
