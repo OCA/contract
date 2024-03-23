fcaddfcb46b4bca10d1e22d966be6aa7960f4239
Christopher Rogos <crogos@gmail.com>
2024-03-23 12:33:38 +0000

    [IMP] contract: black, isort, prettier

--- contract/wizards/contract_contract_terminate.py
+++ contract/wizards/contract_contract_terminate.py
@@ -5,7 +5,6 @@ from odoo import fields, models
 
 
 class ContractContractTerminate(models.TransientModel):
-
     _name = "contract.contract.terminate"
     _description = "Terminate Contract Wizard"
 
