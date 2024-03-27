d1cb9312ae2354823e365718f7dae1beef3b1061
Christopher Rogos <crogos@gmail.com>
2024-03-27 17:12:44 +0000

    [MIG] contract: Migration to 17.0

--- contract/__manifest__.py
+++ contract/__manifest__.py
@@ -11,7 +11,7 @@
 
 {
     "name": "Recurring - Contracts Management",
-    "version": "16.0.2.7.0",
+    "version": "17.0.1.0.0",
     "category": "Contract Management",
     "license": "AGPL-3",
     "author": "Tecnativa, ACSONE SA/NV, Odoo Community Association (OCA)",
@@ -47,11 +47,8 @@
         "views/contract_portal_templates.xml",
     ],
     "assets": {
-        "web.assets_backend": [
-            "contract/static/src/js/section_and_note_fields_backend.js",
-        ],
         "web.assets_frontend": ["contract/static/src/scss/frontend.scss"],
-        "web.assets_tests": ["contract/static/src/js/contract_portal_tour.js"],
+        "web.assets_tests": ["contract/static/src/js/contract_portal_tour.esm.js"],
     },
     "installable": True,
 }
