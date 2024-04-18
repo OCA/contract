3201858f8b65d24e7b7bfacc2d5f9621f2c2a4d6
Luis Rodriguez <luis.rodriguez@dixmit.com>
2024-04-18 17:24:58 +0200

    [MIG] contract_queue_job: Migration to 16.0

--- contract_queue_job/__manifest__.py
+++ contract_queue_job/__manifest__.py
@@ -6,7 +6,7 @@
     "summary": """
         This addon make contract invoicing cron plan each contract in a job
         instead of creating all invoices in one transaction""",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/contract",
