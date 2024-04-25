7332db0548c9c5bfd5cfd4c71bb6ec64a41ee8a0
root <root@16.0>
2024-04-25 07:38:03 +0000

    [IMP]spreadsheet_dashboard_subscription_oca: Add a dashboard to the subscription_oca module

---
+++ spreadsheet_dashboard_subscription_oca/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Spreadsheet dashboard for subscription_oca",
+    "version": "16.0.1.0.0",
+    "category": "Hidden",
+    "summary": "Spreadsheet",
+    "website": "https://github.com/OCA/contract",
+    "depends": ["spreadsheet_dashboard", "subscription_oca"],
+    "author": "Domatix, Odoo Community Association (OCA)",
+    "data": [
+        "data/dashboards.xml",
+    ],
+    "demo": [],
+    "installable": True,
+    "license": "LGPL-3",
+    "assets": {},
+}
