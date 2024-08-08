5216759001f4a8a11c6bb0f263a37e2638cea060
Emilio Pascual <emilio@moduon.team>
2024-08-08 16:15:17 +0200

    [FIX]: contract: load 'mail_notification_contract' if exist inherit view in migration
    @moduon MT-6161

--- contract/migrations/15.0.1.0.0/post-migration.py
+++ contract/migrations/15.0.1.0.0/post-migration.py
@@ -6,12 +6,11 @@ def migrate(env, version):
     openupgrade.load_data(
         env.cr, "contract", "migrations/15.0.1.0.0/noupdate_changes.xml"
     )
+    xml_ids = ["email_contract_template", "mail_template_contract_modification"]
+    if env.ref("mail.mail_notification_paynow", raise_if_not_found=False):
+        xml_ids.append("mail_notification_contract")
     openupgrade.delete_record_translations(
         env.cr,
         "contract",
-        [
-            "email_contract_template",
-            "mail_template_contract_modification",
-            "mail_notification_contract",
-        ],
+        xml_ids,
     )
