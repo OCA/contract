8f23e1aba573061a2dd79c025f81d34daf88af14
Emilio Pascual <emilio@moduon.team>
2024-08-08 15:25:02 +0200

    [FIX]: contract: Migration script check if column old_invoice_line_id exists
    @moduon MT-6161

--- contract/migrations/13.0.1.0.0/post-migration.py
+++ contract/migrations/13.0.1.0.0/post-migration.py
@@ -6,24 +6,27 @@ from openupgradelib import openupgrade  # pylint: disable=W7936
 
 @openupgrade.migrate()
 def migrate(env, version):
-    openupgrade.logged_query(
-        env.cr,
-        """
-        UPDATE account_move am
-        SET old_contract_id = ai.old_contract_id
-        FROM account_invoice ai
-        WHERE ai.id = am.old_invoice_id
-            AND ai.old_contract_id IS NOT NULL""",
-    )
-    openupgrade.logged_query(
-        env.cr,
-        """
-        UPDATE account_move_line aml
-        SET contract_line_id = ail.contract_line_id
-        FROM account_invoice_line ail
-        WHERE ail.id = aml.old_invoice_line_id
-            AND ail.contract_line_id IS NOT NULL""",
-    )
+    if openupgrade.column_exists(
+        env.cr, "account_move", "old_invoice_id"
+    ) and openupgrade.column_exists(env.cr, "account_move_line", "old_invoice_line_id"):
+        openupgrade.logged_query(
+            env.cr,
+            """
+            UPDATE account_move am
+            SET old_contract_id = ai.old_contract_id
+            FROM account_invoice ai
+            WHERE ai.id = am.old_invoice_id
+                AND ai.old_contract_id IS NOT NULL""",
+        )
+        openupgrade.logged_query(
+            env.cr,
+            """
+            UPDATE account_move_line aml
+            SET contract_line_id = ail.contract_line_id
+            FROM account_invoice_line ail
+            WHERE ail.id = aml.old_invoice_line_id
+                AND ail.contract_line_id IS NOT NULL""",
+        )
     openupgrade.load_data(
         env.cr, "contract", "migrations/13.0.1.0.0/noupdate_changes.xml"
     )
