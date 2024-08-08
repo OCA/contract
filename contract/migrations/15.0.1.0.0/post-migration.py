e02b846336f350cdb6f48f2b5f150f747d0d43bf
Emilio Pascual <emilio@moduon.team>
2024-08-08 16:48:06 +0200

    [FIX] contract: xml_id if exists
    @moduon MT-6161

--- contract/migrations/15.0.1.0.0/post-migration.py
+++ contract/migrations/15.0.1.0.0/post-migration.py
@@ -6,12 +6,17 @@ def migrate(env, version):
     openupgrade.load_data(
         env.cr, "contract", "migrations/15.0.1.0.0/noupdate_changes.xml"
     )
-    openupgrade.delete_record_translations(
-        env.cr,
-        "contract",
-        [
-            "email_contract_template",
-            "mail_template_contract_modification",
-            "mail_notification_contract",
-        ],
-    )
+    xml_ids = [
+        "email_contract_template",
+        "mail_template_contract_modification",
+        "mail_notification_contract",
+    ]
+    for xml_id in xml_ids:
+        if env.ref(f"contract.{xml_id}", raise_if_not_found=False):
+            xml_ids.remove("xml_id")
+    if xml_ids:
+        openupgrade.delete_record_translations(
+            env.cr,
+            "contract",
+            xml_ids,
+        )
