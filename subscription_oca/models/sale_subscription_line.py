eeee1e18f5776f10a0afbe612b449a56df7356d6
blueuc <sean@blueuc.com>
2024-11-07 08:41:39 -0500

    Fix: Respect company context for product category default accounts in subscription module

--- subscription_oca/models/sale_subscription_line.py
+++ subscription_oca/models/sale_subscription_line.py
@@ -303,7 +303,8 @@ class SaleSubscriptionLine(models.Model):
         self.ensure_one()
         account = (
             self.product_id.property_account_income_id
-            or self.product_id.categ_id.property_account_income_categ_id
+            # added company context to the product category account defaults for multi-company setups
+            or self.product_id.categ_id.with_context(company_id=self.sale_subscription_id.company_id.id).property_account_income_categ_id
         )
         return {
             "product_id": self.product_id.id,
