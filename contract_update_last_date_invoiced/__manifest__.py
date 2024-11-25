8a58679caed4eda56a5eefe8297120a54874d3f4
Eduardo López Peñalver <elp@moduon.team>
2024-11-25 12:10:27 +0100

    [16.0][MIG] account_update_last_date_invoiced: standard migration

--- contract_update_last_date_invoiced/__manifest__.py
+++ contract_update_last_date_invoiced/__manifest__.py
@@ -5,14 +5,16 @@
     "name": "Contract Last Date Update",
     "summary": """
         This module allows to update the last date invoiced if invoices are deleted.""",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/contract",
     "depends": ["contract"],
     "data": [
         "security/security.xml",
+        "views/contract_views.xml",
         "views/contract_line.xml",
         "wizards/update_last_date_invoiced.xml",
     ],
+    "maintainers": ["rafaelbn", "edlopen"],
 }
--- contract_update_last_date_invoiced/readme/CONTRIBUTORS.rst
+++ contract_update_last_date_invoiced/readme/CONTRIBUTORS.rst
@@ -1,2 +1,4 @@
 * Souheil Bejaoui <souheil.bejaoui@acsone.eu>
 * Denis Roussel <denis.roussel@acsone.eu>
+* `Moduon <https://www.moduon.team>`_:
+  * `Eduardo López`
