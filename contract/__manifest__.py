09b38c79bbe45a866147e900b4904c963d8551b5
Adasat Torres de León <a.torres@binhex.cloud>
2025-02-13 10:57:20 +0000

    [FIX]contract: Malformed expression in the column_invisible attribute.
    [BOT] post-merge updates

--- contract/__manifest__.py
+++ contract/__manifest__.py
@@ -11,7 +11,7 @@
 
 {
     "name": "Recurring - Contracts Management",
-    "version": "17.0.1.4.2",
+    "version": "17.0.1.4.3",
     "category": "Contract Management",
     "license": "AGPL-3",
     "author": "Tecnativa, ACSONE SA/NV, Odoo Community Association (OCA)",
