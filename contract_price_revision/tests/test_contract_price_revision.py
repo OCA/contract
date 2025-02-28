f639315538e9a32b125d9245ff34145f4ade7f30
Alvaro <alvaramar@gmail.com>
2025-02-28 13:24:48 +0000

    [MIG] contract_price_revision: Migration to 18.0
    [FIX] set active_ids to list
    
    [FIX] contract_price_revision: precommit run
    
    [FIX] contract_price_revision: Change dependency

--- contract_price_revision/tests/test_contract_price_revision.py
+++ contract_price_revision/tests/test_contract_price_revision.py
@@ -21,7 +21,9 @@ class TestContractPriceRevision(TestContractBase):
         )
 
     def execute_wizard(self):
-        self.wizard.with_context(active_ids=self.contract.id).action_apply()
+        self.wizard.with_context(
+            active_ids=self.contract.ids, active_model=self.contract._name
+        ).action_apply()
 
     def test_contract_price_revision_wizard(self):
         # This is for checking if this line is not versioned
