883ec746bc953bb1c324500e220bf2bd3a4b3f3f
pilarvargas-tecnativa <pilar.vargas@tecnativa.com>
2025-04-22 12:10:24 +0200

    [IMP] contract: Adapting portal contract design to odoo style
    [BOT] post-merge updates

--- contract/__manifest__.py
+++ contract/__manifest__.py
@@ -11,7 +11,7 @@
 
 {
     "name": "Recurring - Contracts Management",
-    "version": "17.0.1.4.3",
+    "version": "17.0.1.4.4",
     "category": "Contract Management",
     "license": "AGPL-3",
     "author": "Tecnativa, ACSONE SA/NV, Odoo Community Association (OCA)",
