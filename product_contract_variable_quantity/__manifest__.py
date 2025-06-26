5de9cb05982f4518370ce32a2e31e5ab81f7ea22
Benoit Aimont <benoit.aimont@acsone.eu>
2025-06-26 10:40:13 +0200

    Make product_contract_variable_quantity LGPL

--- product_contract_variable_quantity/__manifest__.py
+++ product_contract_variable_quantity/__manifest__.py
@@ -1,12 +1,12 @@
 # Copyright 2018 ACSONE SA/NV
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
     "name": "Product Contract Variable Quantity",
     "summary": """
         Product contract with variable quantity""",
     "version": "18.0.1.0.0",
-    "license": "AGPL-3",
+    "license": "LGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/contract",
     "depends": ["contract_variable_quantity", "product_contract"],
--- product_contract_variable_quantity/tests/test_sale_order.py
+++ product_contract_variable_quantity/tests/test_sale_order.py
@@ -1,5 +1,5 @@
 # Copyright 2018 ACSONE SA/NV.
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 from odoo.tests.common import TransactionCase
 
