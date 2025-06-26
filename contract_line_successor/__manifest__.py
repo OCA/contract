13057edc0fb894bc819a7af80cef8a7bcb2d05ff
Benoit Aimont <benoit.aimont@acsone.eu>
2025-06-26 09:43:44 +0200

    Make contract_line_successor LGPL

--- contract_line_successor/__manifest__.py
+++ contract_line_successor/__manifest__.py
@@ -1,10 +1,10 @@
 # Copyright 2018 ACSONE SA/NV
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
 {
     "name": "Contract Line Successor",
     "version": "18.0.1.0.0",
-    "license": "AGPL-3",
+    "license": "LGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/contract",
     "depends": ["contract"],
