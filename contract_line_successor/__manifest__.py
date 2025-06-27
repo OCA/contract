915e074ed8663da2f6fca4e3930046904c15c22b
Marcel Savegnago <marcel.savegnago@gmail.com>
2025-06-27 15:14:30 +0000

    Added translation using Weblate (Portuguese (Brazil))

--- contract_line_successor/__manifest__.py
+++ contract_line_successor/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Contract Line Successor",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.1",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/contract",
