82a4495da6906160cb8819718bff68ba83e13e44
bo-eska <betulo@eska.biz>
2025-08-14 12:44:32 +0300

    [FIX] product_contract: divide contract configuration button

--- product_contract/__manifest__.py
+++ product_contract/__manifest__.py
@@ -22,5 +22,14 @@
     "application": False,
     "external_dependencies": {"python": ["python-dateutil"]},
     "maintainers": ["sbejaoui"],
-    "assets": {"web.assets_backend": ["product_contract/static/src/js/*"]},
+    "assets": {
+        "web.assets_backend": [
+            "product_contract/static/src/js/*",
+            (
+                "after",
+                "sale/static/src/xml/sale_product_field.xml",
+                "product_contract/static/src/xml/*",
+            ),
+        ]
+    },
 }
