c7e4cc23340ca4534184280776367902839226cb
Yan Chirino <yanpier.chirino@gmail.com>
2025-11-29 14:13:43 -0500

    [MIG] subscription_oca: Migration to 19.0

--- subscription_oca/tests/test_subscription_oca.py
+++ subscription_oca/tests/test_subscription_oca.py
@@ -71,14 +71,20 @@ class TestSubscriptionOCA(BaseCommon):
                 "amount": 0,
             }
         )
-        cls.product_1 = cls.env.ref("product.product_product_5")
-        cls.product_1.list_price = 30.75
-        cls.product_1.subscribable = True
-        cls.product_1.taxes_id = [Command.set(cls.tax_10pc_incl.ids)]
-        cls.product_2 = cls.env.ref("product.product_product_6")
-        cls.product_2.list_price = 38.25
-        cls.product_2.taxes_id = [Command.set(cls.tax_0pc.ids)]
-        cls.product_2.subscribable = True
+        cls.product_1 = cls._create_product(
+            name="product_1",
+            lst_price=30.75,
+            subscribable=True,
+            uom_id=cls.uom_unit.id,
+            taxes_id=[Command.set(cls.tax_10pc_incl.ids)],
+        )
+        cls.product_2 = cls._create_product(
+            name="product_2",
+            lst_price=38.25,
+            subscribable=True,
+            uom_id=cls.uom_unit.id,
+            taxes_id=[Command.set(cls.tax_0pc.ids)],
+        )
 
         cls.country = cls.env["res.country"].search([], limit=1)
         cls.fiscal = cls.env["account.fiscal.position"].create(
