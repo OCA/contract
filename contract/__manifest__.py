46633bc395dab525ea02e99d968f3ca1dc94abe7
Quentin THEURET <quentin@quentin-theuret.net>
2026-01-03 20:01:10 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (258 of 258 strings)
    
    Translation: contract-18.0/contract-18.0-contract
    Translate-URL: https://translation.odoo-community.org/projects/contract-18-0/contract-18-0-contract/fr/

--- contract/__manifest__.py
+++ contract/__manifest__.py
@@ -11,7 +11,7 @@
 
 {
     "name": "Recurring - Contracts Management",
-    "version": "18.0.2.1.3",
+    "version": "18.0.2.2.0",
     "category": "Contract Management",
     "license": "AGPL-3",
     "author": "Tecnativa, ACSONE SA/NV, Odoo Community Association (OCA)",
